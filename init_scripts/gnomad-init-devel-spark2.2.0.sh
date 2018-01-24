e214a7ca86a5bf8a878a79b37ef4544524586e1c
konradjk <konradjkarczewski@gmail.com>
2018-01-24 11:33:39 -0500

    Adding 2.2.0 startup scripts

---
+++ init_scripts/gnomad-init-devel-spark2.2.0.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+PACKAGES="slackclient sklearn tabulate pandas scipy statsmodels"
+pip install --upgrade $PACKAGES
+
+export HAIL_VERSION=devel
+export SPARK_VERSION=2.2.0
+export SPARK_HOME=/usr/lib/spark
+export HAIL_HOME=/hadoop_gcs_connector_metadata_cache/hail
+export HAIL_HASH=$(gsutil cat gs://hail-common/builds/${HAIL_VERSION}/latest-hash-spark-${SPARK_VERSION}.txt)
+export HAIL_JAR=hail-${HAIL_VERSION}-${HAIL_HASH}-Spark-${SPARK_VERSION}.jar
+export HAIL_PYTHON_ZIP=hail-${HAIL_VERSION}-${HAIL_HASH}.zip
+mkdir $HAIL_HOME
+gsutil cp gs://hail-common/builds/${HAIL_VERSION}/jars/${HAIL_JAR} gs://hail-common/builds/${HAIL_VERSION}/python/${HAIL_PYTHON_ZIP} $HAIL_HOME
+
+# Prepare bashrc and redownload script
+cat <<EOT >> /etc/bash.bashrc
+export HAIL_VERSION=${HAIL_VERSION}
+export SPARK_VERSION=${SPARK_VERSION}
+export SPARK_HOME=${SPARK_HOME}
+export HAIL_HOME=${HAIL_HOME}
+export HAIL_HASH=${HAIL_HASH}
+export HAIL_JAR=${HAIL_JAR}
+export HAIL_PYTHON_ZIP=${HAIL_PYTHON_ZIP}
+export _JAVA_OPTIONS='-Xmx8096m'
+export PYTHONPATH=/home/hail:${SPARK_HOME}/python:$(ls ${SPARK_HOME}/python/lib/py4j-*-src.zip):${HAIL_HOME}/${HAIL_PYTHON_ZIP}
+export SPARK_CLASSPATH=${HAIL_HOME}/${HAIL_JAR}
+EOT
+cat <<EOT >> /redownload_hail.sh
+mkdir -p $HAIL_HOME
+gsutil cp gs://hail-common/builds/${HAIL_VERSION}/jars/${HAIL_JAR} gs://hail-common/builds/${HAIL_VERSION}/python/${HAIL_PYTHON_ZIP} $HAIL_HOME
+EOT
+chmod +x /redownload_hail.sh
+
+cd /home/hail/gnomad_hail
+export PYTHONPATH=/home/hail:${SPARK_HOME}/python:$(ls ${SPARK_HOME}/python/lib/py4j-*-src.zip):${HAIL_HOME}/${HAIL_PYTHON_ZIP}
+export SPARK_CLASSPATH=${HAIL_HOME}/${HAIL_JAR}
+
+PYTHONPATH=.:$PYTHONPATH python tests/test_utils.py
+# Use this instead to turn off Slack messages
+# python -m unittest discover &> tests.log
+
+# curl http://www.freefontspro.com/d/14454/arial.zip > /home/anaconda2/lib/python2.7/site-packages/matplotlib/mpl-data/fonts/ttf/arial.zip
+# unzip /home/anaconda2/lib/python2.7/site-packages/matplotlib/mpl-data/fonts/ttf/arial.zip
---
+++ init_scripts/master-init-devel-spark2.2.0.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+ROLE=$(/usr/share/google/get_metadata_value attributes/dataproc-role)
+if [[ "${ROLE}" == 'Master' ]]; then
+
+    mkdir -p /home/hail/
+
+    cd /home/hail
+    git clone https://github.com/macarthur-lab/gnomad_hail.git
+    cd gnomad_hail/init_scripts
+    chmod +x gnomad-init-devel-spark2.2.0.sh sparklyr-init.sh
+
+    # This is here so as not have 2 apt-get processes fighting for a lock
+    apt-get install -y ipython tmux
+
+    ./gnomad-init-devel.sh > gnomad_startup.log 2>&1 &
+    ./sparklyr-init.sh > sparklyr_startup.log 2>&1 &
+
+fi

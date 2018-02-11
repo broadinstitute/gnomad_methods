3850c36260b9f6bd21a4a331e9c2220002997064
konradjk <konradjkarczewski@gmail.com>
2018-02-10 22:02:05 -0500

    functions for qc scripts

--- utils.py
+++ utils.py
@@ -181,6 +181,96 @@ def annotate_adj(vds):
     )
 
 
+def split_multi_sites(vds):
+    sm = hl.SplitMulti(vds)
+    sm.update_rows(a_index=sm.a_index(), was_split=sm.was_split())
+    return sm.result()
+
+
+def split_multi_hardcalls(vds):
+    sm = hl.SplitMulti(vds)
+    sm.update_rows(a_index=sm.a_index(), was_split=sm.was_split())
+    sm.update_entries(functions.downcode(vds.GT))
+    return sm.result()
+
+
+def adjust_sex_ploidy(vds, sex_expr):
+    """
+    Converts males to haploid on non-PAR X/Y, sets females to missing on Y
+
+    :param MatrixTable vds: VDS
+    :param StringExpression sex_expr: Expression pointing to sex in VDS (must be "male" and "female", otherwise no change)
+    :return: MatrixTable with fixed ploidy for sex chromosomes
+    :rtype: MatrixTable
+    """
+    male = sex_expr == 'male'
+    female = sex_expr == 'female'
+    x_nonpar = vds.v.in_x_nonpar
+    y_par = vds.v.in_y_par
+    y_nonpar = vds.v.in_y_nonpar
+    return vds.annotate_entries(
+        GT=case()  # TODO: switch to methods.case
+        .when(female & (y_par | y_nonpar), functions.null(TCall()))
+        .when(male & (x_nonpar | y_nonpar) & vds.GT.is_het(), functions.null(TCall()))
+        .when(male & (x_nonpar | y_nonpar), Call([vds.GT.alleles[0]]))
+        .default(vds.GT)
+    )
+
+
+def get_sample_data(vds, fields, sep='\t', delim='|'):
+    """
+    Hail devs hate this one simple py4j trick to speed up sample queries
+
+    :param MatrixTable vds: MT
+    :param list of StringExpression fields: fields
+    :param sep: Separator to use (tab usually fine)
+    :param delim: Delimiter to use (pipe usually fine)
+    :return: Sample data
+    :rtype: list of str
+    """
+    field_expr = fields[0]
+    for field in fields[1:]:
+        field_expr = field_expr + '|' + field
+    return [x.split(delim) for x in vds.aggregate_cols(x=agg.collect(field_expr).mkstring(sep)).x.split(sep) if x != 'null']
+
+
+def get_popmax_expr(freq):
+    """
+    First pass of popmax (add an additional entry into freq with popmax: pop)
+    TODO: update dict instead?
+
+    :param ArrayStructExpression freq: Array of StructExpression with ['AC', 'AN', 'Hom', 'meta']
+    :return: Frequency data with annotated popmax
+    :rtype: ArrayStructExpression
+    """
+    popmax_entry = (freq
+                    .filter(lambda x: ((x.meta.keys() == ['population']) & (x.meta['population'] != 'oth')))
+                    .sort_by(lambda x: x.AC / x.AN, ascending=False)[0])
+    # return freq.map(lambda x: Struct(AC=x.AC, AN=x.AN, Hom=x.Hom,
+    #                                  meta=functions.cond(
+    #                                      x.meta == popmax_entry.meta,
+    #                                      functions.Dict(x.meta.keys().append('popmax'), x.meta.values().append('True')),  # TODO: update dict
+    #                                      x.meta
+    #                                  )))
+    return freq.append(Struct(AC=popmax_entry.AC, AN=popmax_entry.AN, Hom=popmax_entry.Hom,
+                              meta={'popmax': popmax_entry.meta['population']}))
+
+
+def get_projectmax(vds, loc):
+    """
+    First pass of projectmax (returns aggregated VDS with project_max field)
+
+    :param MatrixTable vds: Array of StructExpression with ['AC', 'AN', 'Hom', 'meta']
+    :return: Frequency data with annotated project_max
+    :rtype: MatrixTable
+    """
+    agg_vds = vds.group_cols_by(loc).aggregate(AC=agg.sum(vds.GT.num_alt_alleles()),
+                                               AN=2 * agg.count_where(functions.is_defined(vds.GT)))
+    agg_vds = agg_vds.annotate_entries(AF=agg_vds.AC / agg_vds.AN)
+    return agg_vds.annotate_rows(project_max=agg.take(Struct(project=agg_vds.s, AC=agg_vds.AC,
+                                                             AF=agg_vds.AF, AN=agg_vds.AN), 5, -agg_vds.AF))
+
+
 def filter_star(vds, a_based=None, r_based=None, g_based=None, additional_annotations=None):
     annotation = unfurl_filter_alleles_annotation(a_based=a_based, r_based=r_based, g_based=g_based,
                                                   additional_annotations=additional_annotations)
@@ -921,6 +1011,349 @@ def quote_field_name(f):
     """
 
     return '`{}`'.format(f) if re.search('^\d|\.', f) else f
+
+# Bootleg:
+
+from hail.expr.expression import unify_types, ExpressionException
+
+class ConditionalBuilder(object):
+    def __init__(self):
+        self._ret_type = None
+        self._cases = []
+
+    def _unify_type(self, t):
+        if self._ret_type is None:
+            self._ret_type = t
+        else:
+            r = unify_types(self._ret_type, t)
+            if not r:
+                raise TypeError("'then' expressions must have same type, found '{}' and '{}'".format(
+                    self._ret_type, t
+                ))
+
+class SwitchBuilder(ConditionalBuilder):
+    """Class for generating conditional trees based on value of an expression.
+
+    Examples
+    --------
+    .. doctest::
+
+        >>> csq = functions.capture('loss of function')
+        >>> expr = (functions.switch(csq)
+        ...                  .when('synonymous', 1)
+        ...                  .when('SYN', 1)
+        ...                  .when('missense', 2)
+        ...                  .when('MIS', 2)
+        ...                  .when('loss of function', 3)
+        ...                  .when('LOF', 3)
+        ...                  .or_missing())
+        >>> eval_expr(expr)
+        3
+
+    Notes
+    -----
+    All expressions appearing as the `then` parameters to
+    :meth:`~.SwitchBuilder.when` or :meth:`~.SwitchBuilder.default` method
+    calls must be the same type.
+
+    See Also
+    --------
+    :func:`.switch`
+
+    Parameters
+    ----------
+    expr : :class:`.Expression`
+        Value to match against.
+    """
+    def __init__(self, base):
+        self._base = functions.to_expr(base)
+        self._has_missing_branch = False
+        super(SwitchBuilder, self).__init__()
+
+    def _finish(self, default):
+        assert len(self._cases) > 0
+
+        from hail.expr.functions import cond, bind
+
+        def f(base):
+            # build cond chain bottom-up
+            expr = default
+            for condition, then in self._cases[::-1]:
+                expr = cond(condition, then, expr)
+            return expr
+
+        return bind(self._base, f)
+
+    def when(self, value, then):
+        """Add a value test. If the `base` expression is equal to `value`, then
+         returns `then`.
+
+        Warning
+        -------
+        Missingness always compares to missing. Both ``NA == NA`` and
+        ``NA != NA`` return ``NA``. Use :meth:`~SwitchBuilder.when_missing`
+        to test missingness.
+
+        Parameters
+        ----------
+        value : :class:`.Expression`
+        then : :class:`.Expression`
+
+        Returns
+        -------
+        :class:`.SwitchBuilder`
+            Mutates and returns `self`.
+        """
+        value = functions.to_expr(value)
+        then = functions.to_expr(then)
+        can_compare = unify_types(self._base.dtype, value.dtype)
+        if not can_compare:
+            raise TypeError("cannot compare expressions of type '{}' and '{}'".format(
+                self._base.dtype, value.dtype))
+
+        self._unify_type(then.dtype)
+        self._cases.append((self._base == value, then))
+        return self
+
+    def when_missing(self, then):
+        """Add a test for missingness. If the `base` expression is missing,
+        returns `then`.
+
+        Parameters
+        ----------
+        then : :class:`.Expression`
+
+        Returns
+        -------
+        :class:`.SwitchBuilder`
+            Mutates and returns `self`.
+        """
+        then = functions.to_expr(then)
+        if self._has_missing_branch:
+            raise ExpressionException("'when_missing' can only be called once")
+        self._unify_type(then.dtype)
+
+        from hail.expr.functions import is_missing
+        # need to insert at 0, because upstream missingness would propagate
+        self._cases.insert(0, (is_missing(self._base), then))
+        return self
+
+    def default(self, then):
+        """Finish the switch statement by adding a default case.
+
+        Notes
+        -----
+        If no value from a :meth:`~.SwitchBuilder.when` call is matched, then
+        `then` is returned.
+
+        Parameters
+        ----------
+        then : :class:`.Expression`
+
+        Returns
+        -------
+        :class:`.Expression`
+        """
+        then = functions.to_expr(then)
+        if len(self._cases) == 0:
+            return then
+        self._unify_type(then.dtype)
+        return self._finish(then)
+
+    def or_missing(self):
+        """Finish the switch statement by returning missing.
+
+        Notes
+        -----
+        If no value from a :meth:`~.SwitchBuilder.when` call is matched, then
+        the result is missing.
+
+        Parameters
+        ----------
+        then : :class:`.Expression`
+
+        Returns
+        -------
+        :class:`.Expression`
+        """
+        if len(self._cases) == 0:
+            raise ExpressionException("'or_missing' cannot be called without at least one 'when' call")
+        from hail.expr.functions import null
+        return self._finish(null(self._ret_type))
+
+
+
+class CaseBuilder(ConditionalBuilder):
+    """Class for chaining multiple if-else statements.
+
+
+    Examples
+    --------
+    .. doctest::
+
+        >>> x = functions.capture('foo bar baz')
+        >>> expr = (functions.case()
+        ...                  .when(x[:3] == 'FOO', 1)
+        ...                  .when(x.length() == 11, 2)
+        ...                  .when(x == 'secret phrase', 3)
+        ...                  .default(0))
+        >>> eval_expr(expr)
+        2
+
+    Notes
+    -----
+    All expressions appearing as the `then` parameters to
+    :meth:`~.CaseBuilder.when` or :meth:`~.CaseBuilder.default` method calls
+    must be the same type.
+
+    See Also
+    --------
+    :func:`.case`
+    """
+    def __init__(self):
+        super(CaseBuilder, self).__init__()
+
+    def _finish(self, default):
+        assert len(self._cases) > 0
+
+        from hail.expr.functions import cond
+
+        expr = default
+        for conditional, then in self._cases[::-1]:
+            expr = cond(conditional, then, expr)
+        return expr
+
+    def when(self, condition, then):
+        """Add a branch. If `condition` is ``True``, then returns `then`.
+
+        Warning
+        -------
+        Missingness is treated similarly to :func:`.cond`. Missingness is
+        **not** treated as ``False``. A `condition` that evaluates to missing
+        will return a missing result, not proceed to the next
+        :meth:`~.CaseBuilder.when` or :meth:`~.CaseBuilder.default`. Always
+        test missingness first in a :class:`.CaseBuilder`.
+
+        Parameters
+        ----------
+        condition: :class:`.BooleanExpression`
+        then : :class:`.Expression`
+
+        Returns
+        -------
+        :class:`.CaseBuilder`
+            Mutates and returns `self`.
+        """
+        condition = functions.to_expr(condition)
+        then = functions.to_expr(then)
+        self._unify_type(then.dtype)
+        self._cases.append((condition, then))
+        return self
+
+    def default(self, then):
+        """Finish the case statement by adding a default case.
+
+        Notes
+        -----
+        If no condition from a :meth:`~.CaseBuilder.when` call is ``True``,
+        then `then` is returned.
+
+        Parameters
+        ----------
+        then : :class:`.Expression`
+
+        Returns
+        -------
+        :class:`.Expression`
+        """
+        then = functions.to_expr(then)
+        if len(self._cases) == 0:
+            return then
+        self._unify_type(then.dtype)
+        return self._finish(then)
+
+    def or_missing(self):
+        """Finish the case statement by returning missing.
+
+        Notes
+        -----
+        If no condition from a :meth:`.CaseBuilder.when` call is ``True``, then
+        the result is missing.
+
+        Parameters
+        ----------
+        then : :class:`.Expression`
+
+        Returns
+        -------
+        :class:`.Expression`
+        """
+        if len(self._cases) == 0:
+            raise ExpressionException("'or_missing' cannot be called without at least one 'when' call")
+        from hail.expr.functions import null
+        return self._finish(null(self._ret_type))
+
+
+def case():
+    """Chain multiple if-else statements with a :class:`.CaseBuilder`.
+
+    Examples
+    --------
+    .. doctest::
+
+        >>> x = functions.capture('foo bar baz')
+        >>> expr = (functions.case()
+        ...                  .when(x[:3] == 'FOO', 1)
+        ...                  .when(x.length() == 11, 2)
+        ...                  .when(x == 'secret phrase', 3)
+        ...                  .default(0))
+        >>> eval_expr(expr)
+        2
+
+    See Also
+    --------
+    :class:`.CaseBuilder`
+
+    Returns
+    -------
+    :class:`.CaseBuilder`.
+    """
+    return CaseBuilder()
+
+
+def switch(expr):
+    """Build a conditional tree on the value of an expression.
+
+    Examples
+    --------
+    .. doctest::
+
+        >>> csq = functions.capture('loss of function')
+        >>> expr = (functions.switch(csq)
+        ...                  .when('synonymous', 1)
+        ...                  .when('SYN', 1)
+        ...                  .when('missense', 2)
+        ...                  .when('MIS', 2)
+        ...                  .when('loss of function', 3)
+        ...                  .when('LOF', 3)
+        ...                  .or_missing())
+        >>> eval_expr(expr)
+        3
+
+    See Also
+    --------
+    :class:`.SwitchBuilder`
+
+    Parameters
+    ----------
+    expr : :class:`.Expression`
+        Value to match against.
+
+    Returns
+    -------
+    :class:`.SwitchBuilder`
+    """
+    return SwitchBuilder(functions.to_expr(expr))
 
 
 def merge_TStructs(s):

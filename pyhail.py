558e26aebb7ab46dd5a22a690763a1b925a64b1e
konradjk <konradjkarczewski@gmail.com>
2018-04-13 13:55:19 -0400

    Adding universal_newlines=True for compatibility with python3 bytestrings

--- pyhail.py
+++ pyhail.py
@@ -51,7 +51,7 @@ def main(args, pass_through_args):
 
     spark_version = '2.2.0' if args.hail_version == 'devel' else '2.0.2'
     if not hash_string:
-        hash_string = subprocess.check_output(['gsutil', 'cat', 'gs://hail-common/builds/{}/latest-hash-spark-{}.txt'.format(args.hail_version, spark_version)]).rstrip()
+        hash_string = subprocess.check_output(['gsutil', 'cat', 'gs://hail-common/builds/{}/latest-hash-spark-{}.txt'.format(args.hail_version, spark_version)], universal_newlines=True).rstrip()
 
     if not hash_string:
         print('Could not get hash string', file=sys.stderr)

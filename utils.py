d7de4a8589fca65ef6b35546d7e654f70b4f60ad
konradjk <konradjkarczewski@gmail.com>
2018-04-29 21:48:06 -0400

    Adding support for haploid GT in unphase_mt

--- utils.py
+++ utils.py
@@ -113,9 +113,13 @@ def annotate_adj(mt: hl.MatrixTable) -> hl.MatrixTable:
 
 def unphase_mt(mt: hl.MatrixTable) -> hl.MatrixTable:
     """
-    Generate unphased version of MatrixTable (assumes call is in mt.GT)
+    Generate unphased version of MatrixTable (assumes call is in mt.GT and is diploid or haploid only)
     """
-    return mt.annotate_entries(GT=hl.call(mt.GT[0], mt.GT[1], phased=False))
+    return mt.annotate_entries(GT=hl.case()
+                               .when(mt.GT.is_diploid(), hl.call(mt.GT[0], mt.GT[1], phased=False))
+                               .when(mt.GT.is_haploid(), hl.call(mt.GT[0], phased=False))
+                               .default(hl.null(hl.tcall))
+    )
 
 
 def add_variant_type(alt_alleles: hl.expr.ArrayExpression) -> hl.expr.StructExpression:

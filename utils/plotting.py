3edd5c0e8ad056b99c0c78304f445c44935edf2d
konradjk <konradjkarczewski@gmail.com>
2018-05-17 15:27:56 -0400

    added typehints

--- utils/plotting.py
+++ utils/plotting.py
@@ -1,7 +1,9 @@
 
+import hail as hl
 import numpy as np
 from ipywidgets import interact
 
+import bokeh
 from bokeh.layouts import gridplot
 from bokeh.plotting import figure, show, output_file
 from bokeh.io import output_notebook, push_notebook, export_png
@@ -19,7 +21,10 @@ from bokeh.models import (
 TOOLS = "hover,save,pan,box_zoom,reset,wheel_zoom"
 
 
-def plot_hail_hist(hist_data, title):
+def plot_hail_hist(hist_data: hl.Struct, title: str) -> bokeh.plotting.Figure:
+    """
+    hist_data can (and should) come straight from ht.aggregate(hl.agg.hist(ht.data, start, end, bins))
+    """
     distance = abs(hist_data.bin_edges[0] - hist_data.bin_edges[1])
     p = figure(title=title)
     p.quad(top=hist_data.bin_freq, bottom=0, left=hist_data.bin_edges[:-1], right=hist_data.bin_edges[1:], fill_color="#036564", line_color="#033649")
@@ -27,7 +32,11 @@ def plot_hail_hist(hist_data, title):
     return p
 
 
-def plot_hail_hist_cumulative(hist_data, title, normalize=True, line_color="#036564", line_width=3):
+def plot_hail_hist_cumulative(hist_data: hl.Struct, title: str, normalize: bool = True,
+                              line_color: str = "#036564", line_width: int = 3) -> bokeh.plotting.Figure:
+    """
+    hist_data can (and should) come straight from ht.aggregate(hl.agg.hist(ht.data, start, end, bins))
+    """
     cumulative_data = np.cumsum(hist_data.bin_freq) + hist_data.n_smaller
     np.append(cumulative_data, [cumulative_data[-1] + hist_data.n_larger])
     num_data_points = max(cumulative_data)

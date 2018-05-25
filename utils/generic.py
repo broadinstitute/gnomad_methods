309f34ce5dd038fc19b28086385a17e47ad29e60
konradjk <konradjkarczewski@gmail.com>
2018-05-25 12:38:15 -0400

    enforcing locus, alleles key for split_multi

--- utils/generic.py
+++ utils/generic.py
@@ -66,17 +66,14 @@ def split_multi_dynamic(t: Union[hl.MatrixTable, hl.Table], keep_star: bool = Fa
     rows = list(t.row)
 
     if isinstance(t, hl.Table):
+        if {'locus', 'alleles'} != set(t.key):
+            raise Exception('Table not keyed by locus and alleles - cannot split_multi')
+
         t = t.annotate(a_index=hl.range(1, hl.len(t.alleles)), was_split=hl.len(t.alleles) > 2)
         t = t.explode('a_index')
 
         new_locus_alleles = hl.min_rep(t.locus, hl.array([t.alleles[0], t.alleles[t.a_index]]))
-        if 'alleles' in t.key:
-            new_keys = {}
-            for k in t.key:
-                new_keys[k] = new_locus_alleles[1] if k == 'alleles' else new_locus_alleles[0] if k == 'locus' else t[k]
-            t = t.key_by(**new_keys)
-        else:
-            t = t.annotate(locus=new_locus_alleles[0], alleles=new_locus_alleles[1])
+        t = t.key_by(**{'locus': new_locus_alleles[0], 'alleles': new_locus_alleles[1]})
 
         if vep_root in rows:
             t = t.annotate(**{vep_root: t[vep_root].annotate(

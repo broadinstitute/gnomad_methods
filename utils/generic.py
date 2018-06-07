78120e40bc5f07cf747d84987238ef0b6704c911
laurent <lfran@broadinstitute.org>
2018-06-07 12:17:11 -0400

    * Added `out_1based_indexing` to `expand_pd_array_col` * Added example in `assign_population_pcs` docstring

--- utils/generic.py
+++ utils/generic.py
@@ -701,16 +701,19 @@ def expand_pd_array_col(
         df: pd.DataFrame,
         array_col: str,
         num_out_cols: int = 0,
-        out_cols_prefix=None
+        out_cols_prefix=None,
+        out_1based_indexing: bool = True
 ) -> pd.DataFrame:
     """
     Expands a Dataframe column containing an array into multiple columns.
 
     :param DataFrame df: input dataframe
     :param str array_col: Column containing the array
+    :param int out_start_index:
     :param int num_out_cols: Number of output columns. If set, only the `n_out_cols` first elements of the array column are output.
                              If <1, the number of output columns is equal to the length of the shortest array in `array_col`
     :param out_cols_prefix: Prefix for the output columns (uses `array_col` as the prefix unless set)
+    :param bool out_1based_indexing: If set, the output column names indexes start at 1. Otherwise they start at 0.
     :return: dataframe with expanded columns
     :rtype: DataFrame
     """
@@ -721,7 +724,7 @@ def expand_pd_array_col(
     if num_out_cols < 1:
         num_out_cols = min([len(x) for x in df[array_col].values.tolist()])
 
-    cols = ['{}{}'.format(out_cols_prefix, i + 1) for i in range(num_out_cols)]
+    cols = ['{}{}'.format(out_cols_prefix, i + out_1based_indexing) for i in range(num_out_cols)]
     df[cols] = pd.DataFrame(df[array_col].values.tolist())[list(range(num_out_cols))]
 
     return df
@@ -743,6 +746,21 @@ def assign_population_pcs(
 
     This function uses a random forest model to assign population labels based on the results of PCA.
     Default values for model and assignment parameters are those used in gnomAD.
+
+    Note that if PCs come from Hail, they will be stored in a single column named `scores` by default.
+    The `expand_pd_array_col` can be used to expand this `scores` column into multiple `PC` columns:
+
+    ```
+        data = pca_ht.to_pandas() #Load PCA results
+        data = expand_pd_array_col(data, 'scores', 10 , 'PC') # Expand `scores` column for 10 PCs into columns `PC1` ... `PC10`
+        results, rf_model = assign_population_pcs(
+            data,
+            ['PC{}'.format(i + 1) for i in range(10)]),
+            ...
+        )
+
+    ```
+
 
     :param Table pop_pc_pd: Pandas dataframe containing population PCs as well as a column with population labels
     :param list of str pc_cols: Columns storing the PCs to use

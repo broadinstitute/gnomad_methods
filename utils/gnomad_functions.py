58b5fd8d70b7e5f1152ecd3afc45f115cbac5527
gtiao <gtiao@broadinstitute.org>
2018-06-25 14:44:26 -0400

    Condensed annotations into one command

--- utils/gnomad_functions.py
+++ utils/gnomad_functions.py
@@ -327,17 +327,17 @@ def add_full_rankings(ht: hl.Table) -> hl.Table:
     # Rank all bi-allelics
     biallelic_ht = ht.filter(ht.was_split, keep=False)
     biallelic_ht = add_rank(biallelic_ht)
-    ht = ht.annotate(biallelic_rank=biallelic_ht[ht.key].rank)
 
     # Rank all singletons
     singleton_ht = ht.filter(ht.info.AC[ht.a_index - 1] == 1)
     singleton_ht = add_rank(singleton_ht)
-    ht = ht.annotate(singleton_rank=singleton_ht[ht.key].rank)
 
     # Rank all bi-allelic singletons
     biallelic_singleton_ht = biallelic_ht.filter(biallelic_ht.info.AC[biallelic_ht.a_index-1] == 1)  # NOTE: we are filtering to singletons across the entire callset, not just in high-quality samples
     biallelic_singleton_ht = add_rank(biallelic_singleton_ht)
-    ht = ht.annotate(biallelic_singleton_rank=biallelic_singleton_ht[ht.key].rank)
+
+    # Annotate and print sanity-check counts
+    ht = ht.annotate(biallelic_rank=biallelic_ht[ht.key].rank, singleton_rank=singleton_ht[ht.key].rank, biallelic_singleton_rank=biallelic_singleton_ht[ht.key].rank)
     print(ht.aggregate(hl.struct(was_split=hl.agg.counter(ht.was_split),
                                  has_biallelic_rank=hl.agg.counter(hl.is_defined(ht.biallelic_rank)),
                                  was_singleton=hl.agg.counter(ht.info.AC[ht.a_index - 1] == 1),

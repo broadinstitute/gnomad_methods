5862d1827b8adf653db8d59da2f156996c67788e
gtiao <gtiao@broadinstitute.org>
2018-07-18 16:19:00 -0400

    Removing zombie code...

--- resources/basics.py
+++ resources/basics.py
@@ -273,7 +273,7 @@ def metadata_genomes_ht_path(version=CURRENT_GENOME_META):
 
 
 def metadata_exomes_ht_path(version=CURRENT_EXOME_META):
-    return 'C'.format(version)
+    return 'gs://gnomad/metadata/exomes/gnomad.exomes.metadata.{0}.ht'.format(version)
 
 
 def coverage_mt_path(data_type) -> str:
@@ -359,27 +359,6 @@ vep_config = "/vep/vep-gcloud.properties"
 
 # Annotations
 context_mt_path = 'gs://gnomad-resources/constraint/context_processed.mt'
-
-
-# Sample QC files
-def qc_mt_path(data_type: str):
-    return 'gs://gnomad/sample_qc/mt/gnomad.{}.high_callrate_common_biallelic_snps.mt'.format(data_type)
-
-
-def qc_ht_path(data_type: str):
-    return 'gs://gnomad/sample_qc/ht/gnomad.{}.high_callrate_common_biallelic_snps.ht'.format(data_type)
-
-
-def qc_temp_data_prefix(data_type: str):
-    return 'gs://gnomad/sample_qc/temp/{0}/gnomad.{0}'.format(data_type)
-
-
-def qc_meta_path(data_type: str):
-    if data_type == 'exomes':
-        return 'gs://gnomad/sample_qc/input_meta/gnomad.exomes.streamlined_metadata.2018-03-21.txt.bgz'
-    else:
-        return 'gs://gnomad/sample_qc/input_meta/gnomad.genomes.streamlined_metadata.2018-03-21.txt.bgz'
-
 
 
 class DataException(Exception):

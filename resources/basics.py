c300d9f95fdba58605a0674e07ebd183c0401b46
konradjk <konradjkarczewski@gmail.com>
2019-03-15 13:21:24 -0400

    can't do relative import

--- resources/basics.py
+++ resources/basics.py
@@ -1,6 +1,6 @@
 import hail as hl
 from typing import *
-from utils.generic import check_hail_file
+from gnomad_hail.utils.generic import check_hail_file
 
 CURRENT_HAIL_VERSION = "0.2"
 CURRENT_RELEASE = "2.1.1"

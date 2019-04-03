391240ae43e9d7f42cf28c61c785aae485638b21
Katherine Chao <kchao@broadinstitute.org>
2019-04-03 18:16:20 -0400

    Updated initial commit with changes requested by Konrad

--- resources/gnomad_liftover.py
+++ resources/gnomad_liftover.py
@@ -1,6 +1,6 @@
 import argparse
-from basics import *
-from gnomad_hail import *
+from gnomad_hail.resources import *
+from gnomad_hail.utils import *
 import logging
 
 
@@ -9,72 +9,41 @@ logger = logging.getLogger("gnomAD_liftover")
 logger.setLevel(logging.INFO)
 
 
-def checkpoint(ht: hl.Table, dtype: str) -> hl.Table:
+def get_checkpoint_path(data_type: str) -> str:
     """
-    Creates a "checkpoint" by writing Table out and reading back in
-    :param Table ht: Table to be evaluated and saved
-    :param str dtype: Data type (exomes or genomes)
-    :return: Table after writing out and reading back in
-    :rtype: Table
+    Creates a checkpoint path for Table
+    :param str data_type: Data type (exomes or genomes)
+    :return: Output checkpoint path
+    :rtype: str
     """
 
-    if dtype == 'exome':
-        ht.write('gs://seqr-datasets/methods_dev/test_data/kc/gnomAD_exome_sites.ht', overwrite=True)
-        ht = hl.read_table('gs://seqr-datasets/methods_dev/test_data/kc/gnomAD_exome_sites.ht')
-
-    else:
-        ht.write('gs://seqr-datasets/methods_dev/test_data/kc/gnomAD_genome_sites.ht', overwrite=True)
-        ht = hl.read_table('gs://seqr-datasets/methods_dev/test_data/kc/gnomAD_genome_sites.ht')
-
-    return ht
+    return f'gs://seqr-datasets/methods_dev/test_data/kc/gnomAD_{data_type}_sites.ht'
 
 
-def lift_ht(ht: hl.Table, dtype: str, rg: hl.genetics.ReferenceGenome) -> hl.Table:
+def lift_ht(ht: hl.Table, data_type: str, rg: hl.genetics.ReferenceGenome) -> hl.Table:
     """
     Lifts gnomAD release table from b37 to b38
     :param Table ht: gnomAD release Table
-    :param str dtype: Data type (exome or genome)
+    :param str data_type: Data type (exome or genome)
     :param ReferenceGenome rg: Reference genome, b38
     :return: gnomAD release Table lifted to b38
     :rtype: Table
     """
 
-    # get reference genomes and load liftover chain file
-    #rg37 = hl.get_reference('GRCh37')
-    #rg38 = rg38 = hl.get_reference('GRCh38')
-    #rg37.add_liftover('gs://hail-common/references/grch37_to_grch38.over.chain.gz', rg38)
-
     # annotate release file with liftover coordinates
     ht = ht.annotate(new_locus = hl.liftover(ht.locus, rg, include_strand=True),
                      old_locus=ht.locus
                     )
-    ht = ht.key_by(locus = ht.new_locus.result)
-    ht = ht.annotate(no_target = hl.is_defined(ht.locus))
-    ht = checkpoint(ht, dtype)
+    ht = ht.key_by(locus = ht.new_locus.result, alleles = ht.alleles)
+    ht = ht.checkpoint(get_checkpoint_path(data_type), overwrite=True)
     return ht
 
 
-def reverse_complement(allele: str):
-    """
-    Returns the reverse complement of an allele
-    :param str allele: Allele to be flipped
-    :return: Reverse complement of input allele
-    :rtype: str
-    """
- 
-    return (hl.switch(allele)
-            .when('A', 'T')
-            .when('T', 'A')
-            .when('G', 'C')
-            .when('C', 'G')
-            .default(allele))
-
-
-def snp_mismatch(ht: hl.Table, dtype: str, rg: hl.genetics.ReferenceGenome) -> hl.Table:
+def snp_mismatch(ht: hl.Table, data_type: str, rg: hl.genetics.ReferenceGenome) -> hl.Table:
     """
     Annotates mismatches between reference allele and allele in reference fasta
     :param Table ht: Table of SNPs to be annotated
-    :param str dtype: Data type (exome or genome)
+    :param str data_type: Data type (exome or genome)
     :param ReferenceGenome rg: GRCh38 reference genome with fasta sequence loaded
     :return: Table annotated with mismatches between reference allele and allele in fasta
     :rtype: Table
@@ -90,26 +59,27 @@ def snp_mismatch(ht: hl.Table, dtype: str, rg: hl.genetics.ReferenceGenome) -> h
     ht = ht.annotate(
         reference_mismatch = hl.cond(
                                 ht.new_locus.is_negative_strand,
-                                (reverse_complement(ht.alleles[0]) != hl.get_sequence(ht.locus.contig, ht.locus.position, reference_genome=rg)),
+                                (generic.flip_base(ht.alleles[0]) != hl.get_sequence(ht.locus.contig, ht.locus.position, reference_genome=rg)),
                                 (ht.alleles[0] != hl.get_sequence(ht.locus.contig, ht.locus.position, reference_genome=rg))
                                 )
                 )
 
-    #ht = checkpoint(ht, dtype)
     return ht
+
  
-def check_mismatch(ht: hl.Table) -> dict:
+def check_mismatch(ht: hl.Table) -> hl.expr.expressions.StructExpression:
     """
     Checks for mismatches between reference allele and allele in reference fasta
     :param Table ht: Table to be checked
-    :return: Dictionary containing mismatch type ('total' or 'minus' as key) and umber of mismatches as value
-    :rtype: dict
+    :return: StructExpression containing counts for mismatches and count for all variants on negative strand
+    :rtype: StructExpression
     """
 
-    mismatch = {}
-    ht = ht.filter(ht.reference_mismatch)
-    mismatch['total'] = ht.count()
-    mismatch['minus'] =  ht.filter(ht.new_locus.is_negative_strand).count()
+    mismatch = ht.aggregate(hl.struct(total_mismatch=hl.agg.count_where(ht.reference_mismatch),
+                                negative_strand=hl.agg.count_where(ht.new_locus.is_negative_strand),
+                                negative_strand_mismatch=hl.agg.count_where(ht.new_locus.is_negative_strand & ht.reference_mismatch)
+                                )
+                            )
     return mismatch
 
 
@@ -119,7 +89,7 @@ def main(args):
     
     # get reference genomes and load liftover chain file
     rg37 = hl.get_reference('GRCh37')
-    rg38 = rg38 = hl.get_reference('GRCh38')
+    rg38 = hl.get_reference('GRCh38')
     rg37.add_liftover('gs://hail-common/references/grch37_to_grch38.over.chain.gz', rg38)
 
     # add fasta sequence to rg38
@@ -128,33 +98,39 @@ def main(args):
             'gs://hail-common/references/Homo_sapiens_assembly38.fasta.fai'
             )
     
-    logger.info('Reading in release ht')
-    if args.type == 'exome':
-        dtype = 'exome'
-        ht = hl.read_table(public_exomes_ht_path(split=True, version=CURRENT_RELEASE))
-    else:
-        dtype = 'genome'
-        ht = hl.read_table(public_genomes_ht_path(split=True, version=CURRENT_RELEASE))
+    if args.exomes:
+        data_type = 'exomes'
+    if args.genomes:
+        data_type = 'genomes'
 
-    logger.info('Working on gnomAD {} release ht'.format(dtype))
+    logger.info('Working on gnomAD {} release ht'.format(data_type))
+    logger.info('Reading in release ht')
+    ht = get_gnomad_public_data(data_type, split=True, version=CURRENT_RELEASE)
+    
     #logger.info('Filtering to chr21 for testing')
     #ht = ht.filter(ht.locus.contig == '21')
 
     logger.info('Lifting ht to b38')
-    ht = lift_ht(ht, dtype, rg38)
+    ht = lift_ht(ht, data_type, rg38)
 
+    ht = hl.read_table(f'gs://seqr-datasets/methods_dev/test_data/kc/gnomAD_{data_type}_sites.ht')
+    
     logger.info('Checking SNPs for reference mismatches')
-    ht = snp_mismatch(ht, dtype, rg38)
+    ht = snp_mismatch(ht, data_type, rg38)
     mismatch = check_mismatch(ht)
-    logger.info('{} reference mismatches in SNPs'.format(mismatch['total']))
-    logger.info('{} mismatches on minus strand'.format(mismatch['minus']))
+    logger.info('{} reference mismatches in SNPs'.format(mismatch['total_mismatch']))
+    logger.info('{} mismatches on minus strand'.format(mismatch['negative_strand_mismatch']))
 
 
 if __name__ == '__main__':
 
     # Create argument parser
-    parser = argparse.ArgumentParser(description='This script lifts gnomad ht to b38')
-    parser.add_argument('-t', '--type', help='Data type', choices=['exome', 'genome'], required=True)
-    #parser.add_argument('-o', '--overwrite', help='Overwrite pre-existing data', action='store_true', default=True)
+    parser = argparse.ArgumentParser(description='This script lifts gnomAD release ht to b38')
+    parser.add_argument('--exomes', help='Data type is exomes. One of --exomes or --genomes is required.', action='store_true')
+    parser.add_argument('--genomes', help='Data type is genomes. One of --exomes or --genomes is required.', action='store_true')
     args = parser.parse_args()
+
+    if int(args.exomes) + int(args.genomes) != 1:
+        sys.exit('Error: One and only one of --exomes or --genomes must be specified')
+
     main(args)

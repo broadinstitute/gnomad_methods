c96d50b9fc4330187a170fe51c996e776e0270e8
Katherine Chao <kchao@broadinstitute.org>
2019-04-10 09:59:57 -0400

    Updated liftover.py with Grace's requested changes

--- resources/liftover.py
+++ resources/liftover.py
@@ -1,7 +1,9 @@
 import argparse
 from gnomad_hail.resources import *
-from gnomad_hail.utils import *
+from gnomad_hail.utils.generic import flip_base 
 import logging
+from os.path import dirname, basename
+import sys
 
 
 logging.basicConfig(format="%(asctime)s (%(name)s %(lineno)s): %(message)s", datefmt='%m/%d/%Y %I:%M:%S %p')
@@ -9,23 +11,30 @@ logger = logging.getLogger("liftover")
 logger.setLevel(logging.INFO)
 
 
-def get_checkpoint_path(gnomad: bool, data_type: str),  -> str:
+def get_checkpoint_path(gnomad: bool, data_type: str, table_path: str) -> str:
     """
     Creates a checkpoint path for Table
+    :param bool gnomad: Whether data is gnomAD data
     :param str data_type: Data type (exomes or genomes for gnomAD; None otherwise)
+    :param str table_path: Path to input Table (if data is not gnomAD data)
     :return: Output checkpoint path
     :rtype: str
     """
+    
+    if gnomad:
+        return get_gnomad_liftover_data_path(data_type, split=True, version=CURRENT_RELEASE)
+    else:
+        out_name = basename(table_path).split('.')[0]
+        return f'{dirname(table_path)}/{out_name}_lift.ht'
 
-    dtype = f'_{data_type}_' if data_type else ''
-    return f'gs://seqr-datasets/methods_dev/test_data/kc/gnomAD_{data_type}_sites.ht'
 
-
-def lift_ht(ht: hl.Table, data_type: str, rg: hl.genetics.ReferenceGenome) -> hl.Table:
+def lift_ht(ht: hl.Table, gnomad: bool, data_type: str, table_path: str,  rg: hl.genetics.ReferenceGenome) -> hl.Table:
     """
     Lifts input Table from one reference build to another
     :param Table ht: Table
+    :param bool gnomad: Whether data is gnomAD data
     :param str data_type: Data type (exome or genome)
+    :param str table_path: Path to input Table (if data is not gnomAD data)
     :param ReferenceGenome rg: Reference genome
     :return: Table with liftover annotations
     :rtype: Table
@@ -36,7 +45,7 @@ def lift_ht(ht: hl.Table, data_type: str, rg: hl.genetics.ReferenceGenome) -> hl
                      old_locus=ht.locus
                     )
     ht = ht.key_by(locus=ht.new_locus.result, alleles = ht.alleles)
-    ht = ht.checkpoint(get_checkpoint_path(data_type), overwrite=True)
+    ht = ht.checkpoint(get_checkpoint_path(gnomad, data_type, table_path), overwrite=True)
     return ht
 
 
@@ -61,7 +70,7 @@ def annotate_snp_mismatch(ht: hl.Table, data_type: str, rg: hl.genetics.Referenc
     ht = ht.annotate(
         reference_mismatch=hl.cond(
                                 ht.new_locus.is_negative_strand,
-                                (generic.flip_base(ht.alleles[0]) != hl.get_sequence(ht.locus.contig, ht.locus.position, reference_genome=rg)),
+                                (flip_base(ht.alleles[0]) != hl.get_sequence(ht.locus.contig, ht.locus.position, reference_genome=rg)),
                                 (ht.alleles[0] != hl.get_sequence(ht.locus.contig, ht.locus.position, reference_genome=rg))
                                 )
                 )
@@ -94,6 +103,8 @@ def main(args):
     rg38 = hl.get_reference('GRCh38')
 
     if args.gnomad:
+        gnomad = True
+        table_path = None
         if not args.exomes and not args.genomes:
             sys.exit('Error: Must specify --exomes or --genomes if lifting gnomAD ht')
 
@@ -108,7 +119,9 @@ def main(args):
 
     else:
         data_type = None
-        ht = hl.read_table(args.table)
+        gnomad = None
+        table_path = args.table_path
+        ht = hl.read_table(table_path)
    
     if args.test: 
         logger.info('Filtering to chr21 for testing')
@@ -117,7 +130,6 @@ def main(args):
         else:
             ht = ht.filter(ht.locus/contig == 'chr21')
 
-    logger.info('Lifting ht to b{}'.format(args.build))
     logger.info('Adding chain file and loading fasta sequence for destination build')
     if args.build == 38:
 
@@ -126,8 +138,10 @@ def main(args):
             'gs://hail-common/references/Homo_sapiens_assembly38.fasta.gz', 
             'gs://hail-common/references/Homo_sapiens_assembly38.fasta.fai'
             )
-        ht = lift_ht(ht, data_type, rg38)
-    
+
+        logger.info('Lifting ht to b{}'.format(args.build))
+        ht = lift_ht(ht, gnomad, data_type, table_path, rg38)
+        
         logger.info('Checking SNPs for reference mismatches')
         ht = annotate_snp_mismatch(ht, data_type, rg38)
 
@@ -137,7 +151,9 @@ def main(args):
             'gs://hail-common/references/human_g1k_v37.fasta.gz',
             'gs://hail-common/references/human_g1k_v37.fasta.fai'
             ) 
-        ht = lift_ht(ht, data_type, rg37)
+
+        logger.info('Lifting ht to b{}'.format(args.build))
+        ht = lift_ht(ht, gnomad, data_type, table_path, rg37)
 
         logger.info('Checking SNPs for reference mismatches')
         ht = annotate_snp_mismatch(ht, data_type, rg37)
@@ -152,8 +168,8 @@ if __name__ == '__main__':
     # Create argument parser
     parser = argparse.ArgumentParser(description='This script lifts a ht from one build to another')
     parser.add_argument('-b', '--build', help='Desired build (37 or 38)', type=int, choices=[37, 38], default="38")
-    parser.add_argument('-t', '--table', help='Full path to table for liftover')
-    parser.add_argument('-g', '--gnomad', help='Liftover table is one of the gnomAD releases')
+    parser.add_argument('-p', '--table_path', help='Full path to table for liftover')
+    parser.add_argument('-g', '--gnomad', help='Liftover table is one of the gnomAD releases', action='store_true')
     parser.add_argument(
             '--exomes', 
             help='Data type is exomes. One of --exomes or --genomes is required if --gnomad is specified.', 
@@ -167,7 +183,10 @@ if __name__ == '__main__':
     parser.add_argument('-t', '--test', help='Filter to chr21 (for code testing purposes)', action='store_true')
     args = parser.parse_args()
 
-    if int(args.exomes) + int(args.genomes) != 1:
+    if args.gnomad and (int(args.exomes) + int(args.genomes) != 1):
         sys.exit('Error: One and only one of --exomes or --genomes must be specified')
+
+    if not args.table_path and not args.gnomad:
+        sys.exit('Error: One and only one of -p/--table_path or -g/--gnomad must be specified')
 
     main(args)

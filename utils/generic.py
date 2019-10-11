4eb2ede1df107e0ae17666ffc3f001bd593cd5bf
laurent <lfran@broadinstitute.org>
2019-10-10 23:25:18 -0400

    VEP struct to CSQ

--- utils/generic.py
+++ utils/generic.py
@@ -1326,3 +1326,71 @@ def fs_from_sb(
         sb_sum > min_count,
         hl.max(0, fs_expr) # Needed to avoid -0.0 values
     )
+
+
+def vep_struct_to_csq(
+        vep_expr: hl.expr.StructExpression,
+        csq_fields: str = "Allele|Consequence|IMPACT|SYMBOL|Gene|Feature_type|Feature|BIOTYPE|EXON|INTRON|"
+                          "HGVSc|HGVSp|cDNA_position|CDS_position|Protein_position|Amino_acids|Codons|"
+                          "ALLELE_NUM|DISTANCE|STRAND|VARIANT_CLASS|MINIMISED|SYMBOL_SOURCE|HGNC_ID|CANONICAL|"
+                          "TSL|APPRIS|CCDS|ENSP|SWISSPROT|TREMBL|UNIPARC|GENE_PHENO|SIFT|PolyPhen|DOMAINS|"
+                          "HGVS_OFFSET|MOTIF_NAME|MOTIF_POS|HIGH_INF_POS|MOTIF_SCORE_CHANGE|LoF|LoF_filter|"
+                          "LoF_flags|LoF_info"
+) -> hl.expr.ArrayExpression:
+    """
+
+    """
+
+    _csq_fields = [f.lower() for f in csq_fields.split("|")]
+
+    def get_csq_from_struct(element: hl.expr.StructExpression, feature_type: str) -> hl.expr.StringExpression:
+        # Most fields are 1-1, just lowercase
+        fields = dict(element)
+
+        # Add general exceptions
+        fields.update({
+            'allele': element.variant_allele,
+            'consequence': hl.delimit(element.consequence_terms, delimiter='&'),
+            'feature_type': feature_type,
+            'feature': (
+                element.transcript_id if 'transcript_id' in element else
+                element.regulatory_feature_id if 'regulatory_feature_id' in element else
+                element.motif_feature_id if 'motif_feature_id' in element else ''
+            ),
+            'variant_class': vep_expr.variant_class
+        })
+
+        # Add exception for transcripts
+        if feature_type == 'Transcript':
+            fields.update({
+                'canonical': hl.cond(element.canonical == 1, 'YES', ''),
+                'ensp': element.protein_id,
+                'gene': element.gene_id,
+                'symbol': element.gene_symbol,
+                'symbol_source': element.gene_symbol_source,
+                'cdna_position': hl.str(element.cdna_start) + hl.cond(element.cdna_start == element.cdna_end, '', "-" + hl.str(element.cdna_end)),
+                'cds_position': hl.str(element.cds_start) + hl.cond(element.cds_start == element.cds_end, '', "-" + hl.str(element.cds_end)),
+                'protein_position': hl.str(element.protein_start) + hl.cond(element.protein_start == element.protein_end, '', "-" + hl.str(element.protein_end)),
+                'sift': element.sift_prediction + "(" + hl.str(element.sift_score) + ")",
+                'polyphen': element.polyphen_prediction + "(" + hl.str(element.polyphen_score) + ")",
+                'domains': hl.delimit(element.domains.map(lambda d: d.db + ":" + d.name), "&")
+            })
+
+        return hl.delimit([hl.or_else(hl.str(fields.get(f, '')), '') for f in _csq_fields], "|")
+
+    csq = hl.empty_array(hl.tstr)
+    for feature_field, feature_type in [
+        ('transcript_consequences', 'Transcript'),
+        ('regulatory_feature_consequences', 'RegulatoryFeature'),
+        ('motif_feature_consequences', 'MotifFeature'),
+        ('intergenic_consequences', 'Intergenic')]:
+        csq = csq.extend(
+            hl.or_else(
+                vep_expr[feature_field].map(
+                    lambda x: get_csq_from_struct(x, feature_type=feature_type)
+                ),
+                hl.empty_array(hl.tstr)
+            )
+        )
+
+    return hl.or_missing(hl.len(csq) >  0, csq)

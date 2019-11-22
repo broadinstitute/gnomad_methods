1d6ba48f6d0d1b9a93627160a2fff3390ce8e000
laurent <lfran@broadinstitute.org>
2019-11-22 12:52:00 +0300

    Tried a few more things in resources: - Used ABC and @abstractmethod - Add warning when creating a resource with unexpected file extension - Removed raw strings - Added PedigreeResource

--- resources/resource_utils.py
+++ resources/resource_utils.py
@@ -1,15 +1,17 @@
 from typing import Optional, Dict, Any
+from gnomad_hail import logger
 import hail as hl
+from abc import ABC, abstractmethod
 
 
 # Resource classes
-class BaseResource:
+class BaseResource(ABC):
     """
     Generic abstract resource class.
     """
 
-
-    def __init__(self, path: str, source_path: Optional[str] = None, attributes: Dict[str, Any] = None):
+    @abstractmethod
+    def __init__(self, path: str, source_path: Optional[str] = None, attributes: Dict[str, Any] = None, expected_file_extension: str = ""):
         """
         Creates a Resource
 
@@ -17,19 +19,20 @@ class BaseResource:
         :param str source_path: The path for the resource source when it was originally imported
         :param dict of str attributes: Additional attributes for the resource
         """
-        if type(self) is BaseResource:
-            raise TypeError("Can't instantiate abstract class BaseResource")
+
+        if expected_file_extension and not path.endswith(expected_file_extension):
+            logger.warning(f"Created the following TableResource with a path that doesn't ends with {expected_file_extension}: {self}")
 
         self.source_path = source_path
         self.path = path
         self.attributes = attributes
 
     def __repr__(self):
-        attr_str = [fr'path={self.path}']
+        attr_str = [f'path={self.path}']
         if self.source_path is not None:
-            attr_str.append(fr'source_path={self.source_path}')
+            attr_str.append(f'source_path={self.source_path}')
         if self.attributes is not None:
-            attr_str.append(fr'attributes={self.attributes}')
+            attr_str.append(f'attributes={self.attributes}')
         return f'{self.__class__.__name__}({",".join(attr_str)})'
 
 
@@ -37,6 +40,15 @@ class TableResource(BaseResource):
     """
     A Hail Table resource
     """
+
+    def __init__(self, path: str, source_path: Optional[str] = None, attributes: Dict[str, Any] = None):
+        super().__init__(
+            path=path,
+            source_path=source_path,
+            attributes=attributes,
+            expected_file_extension='.ht'
+        )
+
     def ht(self) -> hl.Table:
         """
         Read and return the Hail Table resource
@@ -50,6 +62,15 @@ class MatrixTableResource(BaseResource):
     """
     A Hail MatrixTable resource
     """
+
+    def __init__(self, path: str, source_path: Optional[str] = None, attributes: Dict[str, Any] = None):
+        super().__init__(
+            path=path,
+            source_path=source_path,
+            attributes=attributes,
+            expected_file_extension='.mt'
+        )
+
     def mt(self)-> hl.MatrixTable:
         """
         Read and return the Hail MatrixTable resource
@@ -59,7 +80,41 @@ class MatrixTableResource(BaseResource):
         return hl.read_matrix_table(self.path)
 
 
-class BaseVersionedResource(BaseResource):
+class PedigreeResource(BaseResource):
+    """
+    A pedigree resource
+    """
+
+    def __init__(self, path: str, attributes: Dict[str, Any] = None):
+        super().__init__(
+            path=path,
+            source_path=None,
+            attributes=attributes,
+            expected_file_extension='.ped'
+        )
+
+    def ht(self, delimiter=r"\\s+") -> hl.Table:
+        """
+        Reads the pedigree into a family HT using hl.import_fam().
+
+        :param str delimiter: Delimiter used in the ped file
+        :return: Family table
+        :rtype: Table
+        """
+        return hl.import_fam(self.path, delimiter=delimiter)
+
+    def pedigree(self, delimiter=r"\\s+") -> hl.Pedigree:
+        """
+        Reads the pedigree into an hl.Pedigree using hl.Pedigree.read().
+
+        :param str delimiter: Delimiter used in the ped file
+        :return: pedigree
+        :rtype: Pedigree
+        """
+        return hl.Pedigree.read(self.path, delimiter=delimiter)
+
+
+class BaseVersionedResource(BaseResource, ABC):
     """
     Abstract class for a versioned resource
     """
@@ -68,7 +123,7 @@ class BaseVersionedResource(BaseResource):
         """
         Creates a versioned resource.
         The `path`/`source_path` attributes of the versioned resource are those
-        of default version of the resource.
+        of the default version of the resource.
 
         In addition, all versions of the resource are stored in the `versions` attribute.
 
@@ -80,7 +135,7 @@ class BaseVersionedResource(BaseResource):
             raise TypeError("Can't instantiate abstract class BaseVersionedResource")
 
         if default_version not in versions:
-            raise KeyError(f"default_version {default_version} not found in versions dictionary passed to {self.__class__.__name__}.versioned().")
+            raise KeyError(f"default_version {default_version} not found in versions dictionary passed to {self.__class__.__name__}.")
 
         super().__init__(
             path=versions[default_version].path,
@@ -96,6 +151,7 @@ class BaseVersionedResource(BaseResource):
 
 class VersionedTableResource(TableResource, BaseVersionedResource):
     pass
+
 
 class VersionedMatrixTableResource(MatrixTableResource, BaseVersionedResource):
     pass

a33eadbdd31deb8b70535edf67c0ee56e8511fe1
Katherine Chao <kchao@broadinstitute.org>
2020-01-23 15:46:49 -0500

    Added rep_on_read function to generic.py

--- utils/generic.py
+++ utils/generic.py
@@ -1534,4 +1534,18 @@ def interval_length(interval: hl.Interval) -> int:
                 interval.end.position
         )
     else:
-        return interval.end.position - interval.start.position
+        return interval.end.position - interval.start.positio
+
+
+def rep_on_read(path: str, n_partitions: int) -> hl.MatrixTable:
+    """
+    Reparitions a MatrixTable on read
+
+    :param str path: Path to input MatrixTable
+    :param int n_partitions: Number of desired partitions
+    :return: MatrixTable with the number of desired partitions
+    :rtype: hl.MatrixTable
+    """
+    mt = hl.read_matrix_table(path)
+    intervals = mt._calculate_new_partitions(n_partitions)
+    return hl.read_matrix_table(path, _intervals=intervals)

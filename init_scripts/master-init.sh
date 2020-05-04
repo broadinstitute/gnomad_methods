af7e712e2eabd2a6f1d172fb85da507961700256
laurent <lfran@broadinstitute.org>
2020-05-04 16:31:17 -0400

    gnomad_hail -> gnomad_methods in master init script Note: this is already the init script in gs://gnomad-public/tools/inits/

--- init_scripts/master-init.sh
+++ init_scripts/master-init.sh
@@ -9,12 +9,12 @@ pip install numpy
 ROLE=$(/usr/share/google/get_metadata_value attributes/dataproc-role)
 if [[ "${ROLE}" == 'Master' ]]; then
 
-    git clone https://github.com/broadinstitute/gnomad_methods.git /home/gnomad_hail
+    git clone https://github.com/broadinstitute/gnomad_methods.git /home/gnomad_methods
 
     mkdir -p /home/hail/
-    ln -s ln -s /home/gnomad_hail/gnomad /home/hail
+    ln -s /home/gnomad_methods/gnomad /home/hail
 
-    cd /home/gnomad_hail/init_scripts
+    cd /home/gnomad_methods/init_scripts
     chmod +x gnomad-init.sh sparklyr-init.sh
 
     # This is here so as not have 2 apt-get processes fighting for a lock

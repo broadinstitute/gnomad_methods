46803f710fb6ad7d07fc31e25aecf204f1330849
Katherine Chao <kchao@broadinstitute.org>
2021-02-01 12:14:13 -0500

    First pass productionizing generate_gene_lof_matrix

--- gnomad/assessment/summary_stats.py
+++ gnomad/assessment/summary_stats.py
@@ -1,12 +1,14 @@
 import logging
-from typing import Dict
+from typing import Dict, Optional, Set, Union
 
 import hail as hl
 
 from gnomad.utils.filtering import filter_low_conf_regions
 from gnomad.utils.vep import (
+    add_most_severe_consequence_to_consequence,
     filter_vep_to_canonical_transcripts,
     get_most_severe_consequence_for_summary,
+    process_consequences,
 )
 
 
@@ -150,4 +152,181 @@ def get_summary_counts(
     ht = ht.annotate_globals(summary_counts=summary_counts)
     return ht.group_by("freq_bin").aggregate(
         **get_summary_counts_dict(ht.alleles, ht.lof, ht.no_lof_flags)
+    )
+
+
+def get_an_adj_criteria(
+    mt: hl.MatrixTable,
+    samples_by_sex: Optional[Dict[str, int]] = None,
+    meta_root: str = "meta",
+    sex_field: str = "sex_imputation.sex_karyotype",
+    xy_str: str = "XY",
+    xx_str: str = "XX",
+    freq_field: str = "freq",
+    freq_index: int = 0,
+    an_proportion_cutoff: float = 0.8,
+) -> hl.expr.BooleanExpression:
+    """
+    Generates criteria to filter samples based on allele number (AN).
+
+    Uses allele number as proxy for call rate.
+
+    :param mt: Input MatrixTable.
+    :param samples_by_sex: Optional Dictionary containing number of samples (value) for each sample sex (key).
+    :param meta_root: Name of field in MatrixTable containing sample metadata information. Default is 'meta'.
+    :param sex_field: Name of field in MatrixTable containing sample sex assignment. Defualt is 'sex_imputation.sex_karyotype'.
+    :param xy_str: String marking whether a sample has XY sex. Default is 'XY'.
+    :param xx_str: String marking whether a sample has XX sex. Default is 'XX'.
+    :param freq_field: Name of field in MT that contains frequency information. Default is 'freq'.
+    :param freq_index: Which index of frequency struct to use. Default is 0.
+    :param an_proportion_cutoff: Desired allele number proportion cutoff. Default is 0.8.
+    """
+    if samples_by_sex is None:
+        samples_by_sex = mt.aggregate_cols(hl.agg.counter(mt[meta_root][sex_field]))
+    return (
+        hl.case()
+        .when(
+            mt.locus.in_autosome_or_par(),
+            mt[freq_field][freq_index].AN
+            >= an_proportion_cutoff * 2 * sum(samples_by_sex.values()),
+        )
+        .when(
+            mt.locus.in_x_nonpar(),
+            mt[freq_field][freq_index].AN
+            >= an_proportion_cutoff
+            * (samples_by_sex[xy_str] + samples_by_sex[xx_str] * 2),
+        )
+        .when(
+            mt.locus.in_y_nonpar(),
+            mt[freq_field][freq_index].AN
+            >= an_proportion_cutoff * samples_by_sex[xy_str],
+        )
+        .or_missing()
+    )
+
+
+def annotate_tx_expression_data(
+    t: Union[hl.MatrixTable, hl.Table],
+    tx_ht: hl.Table,
+    csq_expr: hl.expr.StructExpression,
+    gene_field: str = "ensg",
+    csq_field: str = "csq",
+    tx_field: str = "tx_annotation",
+):
+    key = t.key if isinstance(t, hl.Table) else t.row_key
+    return hl.find(
+        lambda csq: (csq[gene_field] == csq_expr.gene_id)
+        & (csq[csq_field] == csq_expr.most_severe_consequence),
+        tx_ht[key][tx_field],
+    )
+
+
+def default_generate_gene_lof_matrix(
+    mt: hl.MatrixTable,
+    tx_ht: Optional[hl.Table],
+    filter_field: str = "filters",
+    freq_field: str = "freq",
+    freq_index: int = 0,
+    additional_csq_set: Set[str] = {"missense_variant", "synonymous_variant"},
+    all_transcripts: bool = False,
+    filter_an: bool = False,
+    filter_to_rare: bool = False,
+    pre_loftee: bool = False,
+    lof_csq_set: Set[str] = {
+        "splice_acceptor_variant",
+        "splice_donor_variant",
+        "stop_gained",
+        "frameshift_variant",
+    },
+    remove_ultra_common: bool = False,
+) -> hl.MatrixTable:
+    """
+
+    :param mt: Input MatrixTable.
+    :param tx_ht: Optional Table containing expression levels per transcript.
+    :param filter_field: Name of field in MT that contains variant filters. Default is 'filters'.
+    :param freq_field: Name of field in MT that contains frequency information. Default is 'freq'.
+    :param freq_index: Which index of frequency struct to use. Default is 0.
+    :param additional_csq_set: Set of additional consequences to keep. Default is {'missense_variant', 'synonymous_variant'}.
+    :param all_transcripts: Whether to use all transcripts instead of just the transcript with most severe consequence. Default is False.
+    :param filter_an: Whether to filter using allele number as proxy for call rate. Default is False.
+    :param samples_by_sex: Optional Dictionary containing number of samples (value) for each sample sex (key).
+    :param meta_root: Name of field in MatrixTable containing sample metadata information. Default is 'meta'.
+    :param sex_field: Name of field in MatrixTable containing sample sex assignment. Defualt is 'sex_imputation.sex_karyotype'.
+    :param xy_str: String marking whether a sample has XY sex. Default is 'XY'.
+    :param xx_str: String marking whether a sample has XX sex. Default is 'XX'.
+    :param an_proportion_cutoff: Desired allele number proportion cutoff. Default is 0.8.
+    :param filter_to_rare: Whether to filter to rare (AF < 5%) variants. Default is False.
+    :param pre_loftee: Whether LoF consequences have been annotated with LOFTEE. Default is False.
+    :param lof_csq_set: Set of LoF consequence strings. Default is {"splice_acceptor_variant", "splice_donor_variant", "stop_gained", "frameshift_variant"}.
+    :param remove_ultra_common: Whether to remove ultra common (AF > 95%) variants. Default is False.
+    """
+    filt_criteria = hl.len(mt[filter_field]) == 0
+    if filter_an:
+        filt_criteria &= get_an_adj_criteria(mt)
+    if remove_ultra_common:
+        filt_criteria &= mt[freq_field][freq_index].AF < 0.95
+    if filter_to_rare:
+        filt_criteria &= mt[freq_field][freq_index].AF < 0.05
+    mt = mt.filter_rows(filt_criteria)
+
+    if all_transcripts:
+        explode_field = "transcript_consequences"
+    else:
+        mt = process_consequences(mt)
+        explode_field = "worst_csq_by_gene"
+
+    if pre_loftee:
+        lof_cats = hl.literal(lof_csq_set)
+        criteria = lambda x: lof_cats.contains(
+            add_most_severe_consequence_to_consequence(x).most_severe_consequence
+        )
+    else:
+        criteria = lambda x: (x.lof == "HC") & hl.is_missing(x.lof_flags)
+
+    if additional_csq_set:
+        additional_cats = hl.literal(additional_csq_set)
+        criteria &= lambda x: additional_cats.contains(
+            add_most_severe_consequence_to_consequence(x).most_severe_consequence
+        )
+
+    csqs = mt.vep[explode_field].filter(criteria)
+    mt = mt.select_rows(mt[freq_field], csqs=csqs)
+    mt = mt.explode_rows(mt.csqs)
+    annotation_expr = {
+        "gene_id": mt.csqs.gene_id,
+        "gene": mt.csqs.gene_symbol,
+        "indel": hl.is_indel(mt.alleles[0], mt.alleles[1]),
+        "most_severe_consequence": mt.csqs.most_severe_consequence,
+    }
+
+    if tx_ht:
+        tx_annotation = annotate_tx_expression_data(mt, tx_ht, mt.csqs).mean_expression
+        annotation_expr["expressed"] = (
+            hl.case()
+            .when(tx_annotation >= 0.9, "high")
+            .when(tx_annotation > 0.1, "medium")
+            .when(hl.is_defined(tx_annotation), "low")
+            .default("missing")
+        )
+    else:
+        annotation_expr["transcript_id"] = mt.csqs.transcript_id
+        annotation_expr["canonical"] = hl.is_defined(mt.csqs.canonical)
+    mt = mt.annotate_rows(**annotation_expr)
+
+    return (
+        mt.group_rows_by(*list(annotation_expr.keys()))
+        .aggregate_rows(
+            n_sites=hl.agg.count(),
+            n_sites_array=hl.agg.array_sum(mt.freq.map(lambda x: hl.int(x.AC > 0))),
+            classic_caf=hl.agg.sum(mt[freq_field][freq_index].AF),
+            max_af=hl.agg.max(mt[freq_field][freq_index].AF),
+            classic_caf_array=hl.agg.array_sum(mt[freq_field].map(lambda x: x.AF)),
+        )
+        .aggregate_entries(
+            num_homs=hl.agg.count_where(mt.GT.is_hom_var()),
+            num_hets=hl.agg.count_where(mt.GT.is_het()),
+            defined_sites=hl.agg.count_where(hl.is_defined(mt.GT)),
+        )
+        .result()
     )

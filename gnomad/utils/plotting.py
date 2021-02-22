972998ea9a34cbeba3834d61bccdef1f4267e520
Nick Watts <nwatts@broadinstitute.org>
2021-02-22 12:35:53 -0500

    Resolve Pylint errors

--- gnomad/utils/plotting.py
+++ gnomad/utils/plotting.py
@@ -215,11 +215,14 @@ def plot_multi_hail_hist(
 
         hist_source = ColumnDataSource(data)
 
+        # fmt: off
+        hide_zeros_filter = BooleanFilter(
+            [top > 0 for top in hist_source.data["top"]]  # pylint: disable=unsubscriptable-object
+        )
+        # fmt: on
+
         view = (
-            CDSView(
-                source=hist_source,
-                filters=[BooleanFilter([top > 0 for top in hist_source.data["top"]])],
-            )
+            CDSView(source=hist_source, filters=[hide_zeros_filter])
             if hide_zeros
             else CDSView(source=hist_source)
         )

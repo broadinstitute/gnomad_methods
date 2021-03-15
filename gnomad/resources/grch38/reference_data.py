7b067b675305093ccb26f5dffc5bf7729f278da9
jkgoodrich <jgoodric@broadinstitute.org>
2021-03-15 14:53:28 -0600

    Split before group_by

--- gnomad/resources/grch38/reference_data.py
+++ gnomad/resources/grch38/reference_data.py
@@ -45,11 +45,11 @@ def _import_clinvar(**kwargs) -> hl.Table:
 
 def _import_dbsnp(**kwargs) -> hl.Table:
     dbsnp = import_sites_vcf(**kwargs)
+    # Note: permit_shuffle is set because the dbsnp vcf has duplicate loci (turned into a set) so might be out of order
+    dbsnp = hl.split_multi(dbsnp, permit_shuffle=True)
     dbsnp = dbsnp.group_by(dbsnp.locus, dbsnp.alleles).aggregate(
         rsid=hl.agg.collect_as_set(dbsnp.rsid)
     )
-    # Note: permit_shuffle is set because the dbsnp vcf has duplicate loci (turned into a set) so might be out of order
-    dbsnp = hl.split_multi(dbsnp, permit_shuffle=True)
 
     return dbsnp
 

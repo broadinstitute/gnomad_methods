e95b0b86705f1c7eb7d3016720d5637b9f98d4f1
Katherine Chao <kchao@broadinstitute.org>
2021-03-18 11:50:48 -0400

    Updated make_combo_header_text to return description text if combo_dict length is 1, group is adj, and 'gnomad' is in prefix

--- gnomad/utils/vcf.py
+++ gnomad/utils/vcf.py
@@ -436,6 +436,8 @@ def make_combo_header_text(
 
     if len(combo_dict) == 1:
         if combo_dict["group"] == "adj":
+            if "gnomad" in prefix:
+                return f" {preposition} samples in gnomAD"
             return ""
 
     if "sex" in combo_dict:

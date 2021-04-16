69a51e9787eb9cbd19c50689c1665b134faad6f9
Nick Watts <nwatts@broadinstitute.org>
2021-04-16 14:42:15 -0400

    Use a gnomAD bucket for test paths

--- tests/resources/test_resource_utils.py
+++ tests/resources/test_resource_utils.py
@@ -6,49 +6,53 @@ from gnomad.resources import resource_utils
 class TestTableResource:
     @patch("hail.read_table")
     def test_read_table(self, read_table):
-        resource = resource_utils.TableResource("gs://my-bucket/table.ht")
+        resource = resource_utils.TableResource("gs://gnomad-public/table.ht")
 
         ds = resource.ht()
-        read_table.assert_called_with("gs://my-bucket/table.ht")
+        read_table.assert_called_with("gs://gnomad-public/table.ht")
         assert ds == read_table.return_value
 
 
 class TestMatrixTableResource:
     @patch("hail.read_matrix_table")
     def test_read_matrix_table(self, read_matrix_table):
-        resource = resource_utils.MatrixTableResource("gs://my-bucket/matrix_table.mt")
+        resource = resource_utils.MatrixTableResource(
+            "gs://gnomad-public/matrix_table.mt"
+        )
 
         ds = resource.mt()
-        read_matrix_table.assert_called_with("gs://my-bucket/matrix_table.mt")
+        read_matrix_table.assert_called_with("gs://gnomad-public/matrix_table.mt")
         assert ds == read_matrix_table.return_value
 
 
 class TestPedigreeResource:
     @patch("hail.Pedigree.read")
     def test_read_pedigree(self, read_pedigree):
-        resource = resource_utils.PedigreeResource("gs://my-bucket/pedigree.ped")
+        resource = resource_utils.PedigreeResource("gs://gnomad-public/pedigree.ped")
 
         ds = resource.pedigree()
         read_pedigree.assert_called()
         print(read_pedigree.call_args)
-        assert read_pedigree.call_args[0][0] == "gs://my-bucket/pedigree.ped"
+        assert read_pedigree.call_args[0][0] == "gs://gnomad-public/pedigree.ped"
         assert ds == read_pedigree.return_value
 
     @patch("hail.import_fam")
     def test_read_fam(self, import_fam):
-        resource = resource_utils.PedigreeResource("gs://my-bucket/pedigree.fam")
+        resource = resource_utils.PedigreeResource("gs://gnomad-public/pedigree.fam")
 
         ds = resource.ht()
         import_fam.assert_called()
-        assert import_fam.call_args[0][0] == "gs://my-bucket/pedigree.fam"
+        assert import_fam.call_args[0][0] == "gs://gnomad-public/pedigree.fam"
         assert ds == import_fam.return_value
 
 
 class TestBlockMatrixResource:
     @patch("hail.linalg.BlockMatrix.read")
     def test_read_block_matrix(self, read_block_matrix):
-        resource = resource_utils.BlockMatrixResource("gs://my-bucket/block_matrix.bm")
+        resource = resource_utils.BlockMatrixResource(
+            "gs://gnomad-public/block_matrix.bm"
+        )
 
         ds = resource.bm()
-        read_block_matrix.assert_called_with("gs://my-bucket/block_matrix.bm")
+        read_block_matrix.assert_called_with("gs://gnomad-public/block_matrix.bm")
         assert ds == read_block_matrix.return_value

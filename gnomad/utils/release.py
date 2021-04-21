8ccb770cc622f776af2aef1c0a1a9496f04e5902
gtiao <gtiao@broadinstitute.org>
2021-04-21 11:32:29 -0400

    Update gnomad/utils/release.py
    Co-authored-by: jkgoodrich <33063077+jkgoodrich@users.noreply.github.com>

--- gnomad/utils/release.py
+++ gnomad/utils/release.py
@@ -62,7 +62,7 @@ def make_freq_index_dict(
     :param groups: List of sample groups [adj, raw]. Default is GROUPS.
     :param pops: Dict of sample global population names for gnomAD genomes. Default is POPS.
     :param sexes: List of sample sexes used in VCF export. Default is SEXES
-    :param subset_list: List of sample subsets in dataset. Default is SUBSETS.
+    :param subset_list: List of sample subsets in dataset. Default is SUBSETS
     :param downsamplings: List of downsampling cohort sizes present in global frequency array
     :param label_delimiter: String used as delimiter when making group label combinations.
     :return: Dictionary keyed by the grouping combinations found in the frequency array, where values are the corresponding

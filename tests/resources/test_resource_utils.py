6edcb61afedeb9a365c0b383caf2222977b1639f
Nick Watts <nwatts@broadinstitute.org>
2021-04-26 14:25:21 -0400

    Run pydocstyle on tests

--- tests/resources/test_resource_utils.py
+++ tests/resources/test_resource_utils.py
@@ -1,11 +1,16 @@
+"""Tests for resource classes."""
+
 from unittest.mock import patch
 
 from gnomad.resources import resource_utils
 
 
 class TestTableResource:
+    """Tests for TableResource."""
+
     @patch("hail.read_table")
     def test_read_table(self, read_table):
+        """Test that Table is read from path."""
         resource = resource_utils.TableResource("gs://gnomad-public/table.ht")
 
         ds = resource.ht()
@@ -14,8 +19,11 @@ class TestTableResource:
 
 
 class TestMatrixTableResource:
+    """Tests for MatrixTableResource."""
+
     @patch("hail.read_matrix_table")
     def test_read_matrix_table(self, read_matrix_table):
+        """Test that MatrixTable is read from path."""
         resource = resource_utils.MatrixTableResource(
             "gs://gnomad-public/matrix_table.mt"
         )
@@ -26,8 +34,11 @@ class TestMatrixTableResource:
 
 
 class TestPedigreeResource:
+    """Tests for PedigreeResource."""
+
     @patch("hail.Pedigree.read")
     def test_read_pedigree(self, read_pedigree):
+        """Test that Pedigree is read from path."""
         resource = resource_utils.PedigreeResource("gs://gnomad-public/pedigree.ped")
 
         ds = resource.pedigree()
@@ -38,6 +49,7 @@ class TestPedigreeResource:
 
     @patch("hail.import_fam")
     def test_read_fam(self, import_fam):
+        """Test that Table is imported from path."""
         resource = resource_utils.PedigreeResource("gs://gnomad-public/pedigree.fam")
 
         ds = resource.ht()
@@ -47,8 +59,11 @@ class TestPedigreeResource:
 
 
 class TestBlockMatrixResource:
+    """Tests for BlockMatrixResource."""
+
     @patch("hail.linalg.BlockMatrix.read")
     def test_read_block_matrix(self, read_block_matrix):
+        """Test that BlockMatrix is read from path."""
         resource = resource_utils.BlockMatrixResource(
             "gs://gnomad-public/block_matrix.bm"
         )

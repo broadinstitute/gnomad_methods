5e169b202e3356e41399661f4cac0438ac4be2c5
jkgoodrich <jgoodric@broadinstitute.org>
2021-05-20 14:57:21 -0600

    Add generalized functions from gnomad_qc vcf export

--- gnomad/utils/filtering.py
+++ gnomad/utils/filtering.py
@@ -341,3 +341,17 @@ def filter_to_clinvar_pathogenic(
         t.count_rows() if isinstance(t, hl.MatrixTable) else t.count(),
     )
     return t
+
+
+def remove_fields_from_globals(global_field: List[str], fields_to_remove: List[str]):
+    """
+    Remove fields from the pre-defined global field variables.
+
+    :param global_field: Global list of fields
+    :param fields_to_remove: List of fields to remove from global (they must be in the global list)
+    """
+    for field in fields_to_remove:
+        if field in global_field:
+            global_field.remove(field)
+        else:
+            logger.info(f"'{field}'' missing from {global_field}")
--- gnomad/utils/vcf.py
+++ gnomad/utils/vcf.py
@@ -493,6 +493,31 @@ def make_combo_header_text(
     return header_text
 
 
+def create_label_groups(
+    pops: List[str],
+    sexes: List[str],
+    groups: List[str] = GROUPS,
+    pop_sex_groups: List[str] = ["adj"],
+) -> List[Dict[str, List[str]]]:
+    """
+    Generate a list of label group dictionaries needed to populate info dictionary.
+
+    Label dictionaries are passed as input to `make_info_dict`.
+
+    :param pops: List of population names.
+    :param sexes: List of sample sexes.
+    :param groups: List of data types (raw, adj). Default is ["raw", "adj"].
+    :param pop_sex_groups: List of only data types to populate with pops and sexes (raw, adj). Default is ["adj"].
+    :return: List of label group dictionaries.
+    """
+    return [
+        dict(group=groups),
+        dict(group=pop_sex_groups, sex=sexes),
+        dict(group=pop_sex_groups, pop=pops),
+        dict(group=pop_sex_groups, pop=pops, sex=sexes),
+    ]
+
+
 def make_info_dict(
     prefix: str = "",
     prefix_before_metric: bool = True,
@@ -865,3 +890,64 @@ def set_female_y_metrics_to_na(
             }
         )
     return female_metrics_dict
+
+
+def build_vcf_export_reference(
+    name: str,
+    build: str = "GRCh38",
+    contigs_keep: List[str] = [f"chr{i}" for i in range(1, 23)]
+    + ["chrX", "chrY", "chrM"],
+) -> hl.ReferenceGenome:
+    """
+    Create export reference based on reference genome defined by `build`.
+
+    By default this will return a new reference with all non-standard contigs eliminated. Keeps chr 1-22, Y, X, and M.
+
+    Non-standard for example is: ##contig=<ID=chr3_GL000221v1_random,length=155397,assembly=GRCh38>
+
+    :param name: Name to use for new reference.
+    :param build: Reference genome build to use as starting reference genome.
+    :param contigs_keep: Contigs to keep from reference genome defined by `build`.
+    :return: Reference genome for VCF export containing only contigs in `contigs_keep`
+    """
+    ref = hl.get_reference(build)
+
+    export_reference = hl.ReferenceGenome(
+        name=name,
+        contigs=contigs_keep,
+        lengths={contig: ref.lengths[contig] for contig in contigs_keep},
+        x_contigs=ref.x_contigs,
+        y_contigs=ref.y_contigs,
+        par=[
+            (interval.start.contig, interval.start.position, interval.end.position)
+            for interval in ref.par
+        ],
+        mt_contigs=ref.mt_contigs,
+    )
+
+    return export_reference
+
+
+def rekey_new_reference(
+    t: Union[hl.Table, hl.MatrixTable], reference: hl.ReferenceGenome
+):
+    """
+    Re-key Table or MatrixTable with a new reference genome.
+
+    :param t: Input Table/MatrixTable.
+    :param reference: Reference genome to re-key with.
+    :return: Re-keyed Table/MatrixTable
+    """
+    t = t.rename({"locus": "locus_original"})
+    locus_expr = hl.locus(
+        t.locus_original.contig, t.locus_original.position, reference_genome=reference,
+    )
+
+    if isinstance(t, hl.MatrixTable):
+        t = t.annotate_rows(locus=locus_expr)
+        t = t.key_rows_by("locus", "alleles").drop("locus_original")
+    else:
+        t = t.annotate(locus=locus_expr)
+        t = t.key_by("locus", "alleles").drop("locus_original")
+
+    return t

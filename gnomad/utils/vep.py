725d4a5519046a97a015169ce30ded9f68bfeef4
averywpx <51382435+averywpx@users.noreply.github.com>
2022-10-14 09:57:02 -0400

    Add generic constraint functions: annotate_with_mu(), count_variants(), downsampling_counts_expr(), filter_vep_transcript_csqs(), combine_functions(), filter_x_nonpar(), and filter_y_nonpar() (#481)

--- gnomad/utils/vep.py
+++ gnomad/utils/vep.py
@@ -9,6 +9,7 @@ from typing import List, Optional, Union
 import hail as hl
 
 from gnomad.resources.resource_utils import VersionedTableResource
+from gnomad.utils.filtering import combine_functions
 
 logging.basicConfig(format="%(levelname)s (%(name)s %(lineno)s): %(message)s")
 logger = logging.getLogger(__name__)
@@ -344,32 +345,38 @@ def process_consequences(
 
 
 def filter_vep_to_canonical_transcripts(
-    mt: Union[hl.MatrixTable, hl.Table], vep_root: str = "vep"
+    mt: Union[hl.MatrixTable, hl.Table],
+    vep_root: str = "vep",
+    filter_empty_csq: bool = False,
 ) -> Union[hl.MatrixTable, hl.Table]:
-    """Filter VEP transcript consequences to those in the canonical transcript."""
-    canonical = mt[vep_root].transcript_consequences.filter(
-        lambda csq: csq.canonical == 1
-    )
-    vep_data = mt[vep_root].annotate(transcript_consequences=canonical)
-    return (
-        mt.annotate_rows(**{vep_root: vep_data})
-        if isinstance(mt, hl.MatrixTable)
-        else mt.annotate(**{vep_root: vep_data})
+    """
+    Filter VEP transcript consequences to those in the canonical transcript.
+
+    :param mt: Input Table or MatrixTable.
+    :param vep_root: Name used for VEP annotation. Default is 'vep'.
+    :param filter_empty_csq: Whether to filter out rows where 'transcript_consequences' is empty. Default is False.
+    :return: Table or MatrixTable with VEP transcript consequences filtered.
+    """
+    return filter_vep_transcript_csqs(
+        mt, vep_root, synonymous=False, filter_empty_csq=filter_empty_csq
     )
 
 
 def filter_vep_to_synonymous_variants(
-    mt: Union[hl.MatrixTable, hl.Table], vep_root: str = "vep"
+    mt: Union[hl.MatrixTable, hl.Table],
+    vep_root: str = "vep",
+    filter_empty_csq: bool = False,
 ) -> Union[hl.MatrixTable, hl.Table]:
-    """Filter VEP transcript consequences to those with a most severe consequence of synonymous_variant."""
-    synonymous = mt[vep_root].transcript_consequences.filter(
-        lambda csq: csq.most_severe_consequence == "synonymous_variant"
-    )
-    vep_data = mt[vep_root].annotate(transcript_consequences=synonymous)
-    return (
-        mt.annotate_rows(**{vep_root: vep_data})
-        if isinstance(mt, hl.MatrixTable)
-        else mt.annotate(**{vep_root: vep_data})
+    """
+    Filter VEP transcript consequences to those with a most severe consequence of 'synonymous_variant'.
+
+    :param mt: Input Table or MatrixTable.
+    :param vep_root: Name used for VEP annotation. Default is 'vep'.
+    :param filter_empty_csq: Whether to filter out rows where 'transcript_consequences' is empty. Default is False.
+    :return: Table or MatrixTable with VEP transcript consequences filtered.
+    """
+    return filter_vep_transcript_csqs(
+        mt, vep_root, canonical=False, filter_empty_csq=filter_empty_csq
     )
 
 
@@ -560,6 +567,54 @@ def get_most_severe_consequence_for_summary(
         )
         .default(_get_most_severe_csq(ht.vep.intergenic_consequences, False))
     )
+
+
+def filter_vep_transcript_csqs(
+    t: Union[hl.Table, hl.MatrixTable],
+    vep_root: str = "vep",
+    synonymous: bool = True,
+    canonical: bool = True,
+    filter_empty_csq: bool = True,
+) -> Union[hl.Table, hl.MatrixTable]:
+    """
+    Filter VEP transcript consequences based on specified criteria, and optionally filter to variants where transcript consequences is not empty after filtering.
+
+    Transcript consequences can be filtered to those where 'most_severe_consequence' is 'synonymous_variant' and/or the transcript is the canonical transcript,
+    if the `synonymous` and `canonical` parameter are set to True, respectively.
+
+    If `filter_empty_csq` parameter is set to True, the Table/MatrixTable is filtered to variants where 'transcript_consequences' within the VEP annotation
+    is not empty after the specified filtering criteria is applied.
+
+    :param t: Input Table or MatrixTable.
+    :param vep_root: Name used for VEP annotation. Default is 'vep'.
+    :param synonymous: Whether to filter to variants where the most severe consequence is 'synonymous_variant'. Default is True.
+    :param canonical: Whether to filter to only canonical transcripts. Default is True.
+    :param filter_empty_csq: Whether to filter out rows where 'transcript_consequences' is empty, after filtering 'transcript_consequences' to the specified criteria. Default is True.
+    :return: Table or MatrixTable filtered to specified criteria.
+    """
+    if not synonymous and not canonical and not filter_empty_csq:
+        logger.warning("No changes have been made to input Table/MatrixTable!")
+        return t
+
+    transcript_csqs = t[vep_root].transcript_consequences
+    criteria = [lambda csq: True]
+    if synonymous:
+        criteria.append(lambda csq: csq.most_severe_consequence == "synonymous_variant")
+    if canonical:
+        criteria.append(lambda csq: csq.canonical == 1)
+    transcript_csqs = transcript_csqs.filter(lambda x: combine_functions(criteria, x))
+    is_mt = isinstance(t, hl.MatrixTable)
+    vep_data = {vep_root: t[vep_root].annotate(transcript_consequences=transcript_csqs)}
+    t = t.annotate_rows(**vep_data) if is_mt else t.annotate(**vep_data)
+
+    if filter_empty_csq:
+        transcript_csq_expr = t[vep_root].transcript_consequences
+        filter_expr = hl.is_defined(transcript_csq_expr) & (
+            hl.len(transcript_csq_expr) > 0
+        )
+        t = t.filter_rows(filter_expr) if is_mt else t.filter(filter_expr)
+
+    return t
 
 
 def add_most_severe_csq_to_tc_within_vep_root(

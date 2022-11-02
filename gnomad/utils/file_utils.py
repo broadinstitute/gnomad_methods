2199db3a1b12a6a6a9bd87372f4c3bbf72ebaa57
Katherine Chao <kchao@broadinstitute.org>
2022-11-02 10:59:50 -0400

    temp tqdm fix

--- gnomad/utils/file_utils.py
+++ gnomad/utils/file_utils.py
@@ -14,8 +14,7 @@ import hail as hl
 from hailtop.aiogoogle import GoogleStorageAsyncFS
 from hailtop.aiotools import AsyncFS, LocalAsyncFS
 from hailtop.aiotools.router_fs import RouterAsyncFS
-from hailtop.utils import bounded_gather
-from hailtop.utils.rich_progress_bar import SimpleRichProgressBar
+from hailtop.utils import bounded_gather, tqdm
 
 from gnomad.resources.resource_utils import DataException
 
@@ -56,8 +55,8 @@ async def parallel_file_exists_async(
         else:
             return True
 
-    with SimpleRichProgressBar(
-        total=len(fpaths), description="check files for existence", disable=False
+    with tqdm(
+        total=len(fpaths), desc="check files for existence", disable=False
     ) as pbar:
         with ThreadPoolExecutor() as thread_pool:
             async with RouterAsyncFS(

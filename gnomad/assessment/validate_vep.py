0781a03c9ef8ed942534d34283edd7497ba8aa0f
heqin <heqin@broadinstitute.org>
2023-05-25 15:56:43 -0400

    reorganize code to small modules

--- gnomad/assessment/validate_vep.py
+++ gnomad/assessment/validate_vep.py
@@ -13,9 +13,54 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-def get_variant_count_in_protein_coding_gene(
+def count_variant_per_interval(i_ht: hl.Table(), vep_ht: hl.Table()) -> hl.Table():
+    """
+    Count how many variants in each interval.
+
+    :param i_ht: hl.Table(), interval information of protein-coding genes, downloaded from Ensembl Archive for 101 & 105
+    :param vep_ht: hl.Table(), VEP-annotated HT, only selected the vep.transcript_consequences field
+    :return: hl.Table(), interval information with variant count
+    """
+    # convert the interval information to locus_interval in Hail format
+    i_ht = i_ht.key_by(
+        interval=hl.locus_interval(
+            hl.literal("chr") + hl.str(i_ht.chr),
+            i_ht.start,
+            i_ht.end,
+            reference_genome="GRCh38",
+        )
+    )
+
+    # join the interval information with the VEP-annotated HT by locus
+    i_ht = i_ht.annotate(interval_copy=i_ht.interval)
+    vep_ht = vep_ht.annotate(
+        the_interval_or_NA=i_ht[vep_ht.locus].interval_copy
+    )  # join by locus
+    vep_ht = vep_ht.filter(hl.is_defined(vep_ht.the_interval_or_NA))
+    vep_ht = vep_ht.annotate(the_interval=vep_ht.the_interval_or_NA)
+
+    # count the number of total variants and "protein-coding" variants in each interval
+    re = vep_ht.group_by(vep_ht.the_interval).aggregate(
+        all_variants=hl.agg.count(),
+        variants_in_pcg=hl.agg.count_where(
+            vep_ht.transcript_consequences.biotype.contains("protein_coding")
+        ),
+    )
+    i_ht = i_ht.annotate(
+        all_variants=re[i_ht.interval].all_variants,
+        variants_in_pcg=re[i_ht.interval].variants_in_pcg,
+    )
+    na_genes = i_ht.filter(hl.is_missing(i_ht.all_variants))
+    logger.info(
+        f"{len(na_genes).gene_stable_ID.collect()} gene(s) have no variants annotated"
+        " as protein-coding in biotype."
+    )
+    return i_ht
+
+
+def import_filter_vep_data(
     release: str("v4.0"), dataset, vep_version: str("105"), test=False
-):
+) -> hl.Table():
     """
     Count how many variants annotated by VEP in each protein-coding gene according to its interval information.
 
@@ -49,15 +94,13 @@ def get_variant_count_in_protein_coding_gene(
     if vep_version not in ["101", "105"]:
         logger.warning("vep_version should be 101 or 105 only")
 
-    startTime = datetime.now()
-
     # import interval information of protein-coding genes
     if test == True:
-        i = hl.import_table(
+        i_ht = hl.import_table(
             "gs://gnomad-qin/test_3pc_genes.tsv", delimiter="\t", impute=True
         )
     else:
-        i = hl.import_table(
+        i_ht = hl.import_table(
             f"gs://gnomad-qin/ensembl_v{vep_version}_pc_genes.tsv",
             delimiter="\t",
             impute=True,
@@ -65,15 +108,15 @@ def get_variant_count_in_protein_coding_gene(
 
     # define the path of the VEP-annotated HT
     if release == "v4.0" and dataset == "exomes":
-        t = hl.read_table(
+        vep_ht = hl.read_table(
             "gs://gnomad/v4.0/annotations/exomes/gnomad.exomes.v4.0.vep.ht"
         )
     elif release == "v4.0" and dataset == "genomes":
-        t = hl.read_table(
+        vep_ht = hl.read_table(
             "gs://gnomad/v4.0/annotations/genomes/gnomad.genomes.v4.0.vep.ht"
         )
     elif release == "v3.1" and dataset == "genomes":
-        t = hl.read_table(
+        vep_ht = hl.read_table(
             "gs://gnomad/release/3.1.4/ht/genomes/gnomad.genomes.v3.1.4.sites.ht"
         )
     else:
@@ -81,86 +124,52 @@ def get_variant_count_in_protein_coding_gene(
             "currently only support v4.0 exomes, v4.0 genomes and v3.1 genomes"
         )
 
-    # convert the interval information to locus_interval in Hail format
-    i = i.key_by(
-        interval=hl.locus_interval(
-            hl.literal("chr") + hl.str(i.chr), i.start, i.end, reference_genome="GRCh38"
-        )
-    )
+    vep_ht = vep_ht.select(vep_ht.vep.transcript_consequences)
 
-    i = i.checkpoint(
-        f"gs://gnomad-tmp-4day/interval_info_{release}_{dataset}_{vep_version}.ht",
-        overwrite=True,
-    )
+    return i_ht, vep_ht
 
-    # join the interval information with the VEP-annotated HT by locus
-    i = i.annotate(interval_copy=i.interval)
-    t = t.annotate(the_interval_or_NA=i[t.locus].interval_copy)  # join by locus
-    t = t.filter(hl.is_defined(t.the_interval_or_NA))
-    t = t.annotate(the_interval=t.the_interval_or_NA)
 
-    # count the number of total variants and "protein-coding" variants in each interval
-    re = t.group_by(t.the_interval).aggregate(
-        all_variants=hl.agg.count(),
-        variants_in_pcg=hl.agg.count_where(
-            t.vep.transcript_consequences.biotype.contains("protein_coding")
-        ),
-    )
+def filter_na_genes(i_ht: hl.Table()) -> hl.Table():
+    """Filter genes with no variants annotated as protein-coding in biotype.
 
-    re = re.checkpoint(
-        f"gs://gnomad-tmp-4day/interval_count_{release}_{dataset}_{vep_version}.ht",
-        overwrite=True,
-    )
-
-    i = i.annotate(
-        all_variants=re[i.interval].all_variants,
-        variants_in_pcg=re[i.interval].variants_in_pcg,
-    )
-
-    i = i.checkpoint(
-        f"gs://gnomad-tmp-4day/interval_info_with_count_{release}_{dataset}_{vep_version}.ht",
-        overwrite=True,
-    )
-
-    # output the number of genes and the name of genes
-    na_genes = i.filter(hl.is_missing(i.all_variants)).gene_stable_ID.collect()
+    :param i_ht: hl.Table, interval HT with count of variants per gene.
+    """
+    na_genes = i_ht.filter(hl.is_missing(i_ht.all_variants))
     logger.info(
-        f"{len(na_genes)} gene(s) have no overlapped intervals with this gnomAD release"
+        "Filtering genes with no variants annotated as protein-coding in biotype."
     )
-
-    variant0_genes = i.filter(i.all_variants == 0).gene_stable_ID.collect()
-    logger.info(
-        f"{len(variant0_genes)} gene(s) have their intervals overlapped but have no"
-        " variants found in their defined intervals"
-    )
-
-    non_pcg_genes = i.filter(
-        (i.all_variants != 0) & (i.variants_in_pcg == 0)
-    ).gene_stable_ID.collect()
-    logger.info(
-        f"{len(non_pcg_genes)} gene(s) have variants found in overlapped interval"
-        " but no variants annotated as protein-coding"
-        f" biotype in their intervals, including {non_pcg_genes}"
-    )
-
-    partial_pcg_genes = i.filter(
-        (i.all_variants != 0)
-        & (i.variants_in_pcg != 0)
-        & (i.all_variants != i.variants_in_pcg)
-    ).gene_stable_ID.collect()
-    logger.info(
-        f"{len(partial_pcg_genes)} gene(s) have a subset of variants annotated as"
-        " protein-coding biotype in their defined intervals"
-    )
-
-    logger.info(f"Time elapsed: {datetime.now() - startTime}")
+    return na_genes
 
 
 def main(args):
     """Validate number of variants by VEP in protein-coding genes."""
-    get_variant_count_in_protein_coding_gene(
+    hl.init(log="/tmp/hail.log", tmp_dir="gs://gnomad-tmp-4day")
+    startTime = datetime.now()
+    logger.info(
+        "importing vep_HT on dataset:"
+        f" gnomAD_{args.release}_{args.dataset}_vep{args.vep_version}"
+    )
+    i_ht, vep_ht = import_filter_vep_data(
         args.release, args.dataset, args.vep_version, args.test
     )
+    logger.info(f"counting variants in each protein-coding gene: 1st round")
+    i_ht = count_variant_per_interval(i_ht, vep_ht)
+    logger.info(
+        f"filtering genes with no variants annotated as protein-coding in biotype"
+    )
+    na_genes = filter_na_genes(i_ht)
+    logger.info(f"counting variants in each protein-coding gene: 2nd round")
+    na_genes_counted = count_variant_per_interval(na_genes)
+    logger.info(f"combining 2 rounds of counting")
+    i_ht = i_ht.annotate(
+        all_variants=na_genes_counted[i_ht.interval].all_variants,
+        variants_in_pcg=na_genes_counted[i_ht.interval].variants_in_pcg,
+    )
+    i_ht.checkpoint(
+        f"gs://gnomad-qin/validate_vep_results/interval_info_with_count_{args.release}_{args.dataset}_vep{args.vep_version}.ht",
+        overwrite=True,
+    )
+    logger.info(f"Time elapsed: {datetime.now() - startTime}")
 
 
 if __name__ == "__main__":

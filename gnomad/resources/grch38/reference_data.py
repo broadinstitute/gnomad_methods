431be529beacbc1583a7fae4cb762c38016ddd94
klaricch <kristen@broadinstitute.org>
2023-10-12 11:13:13 -0400

    Update gnomad/resources/grch38/reference_data.py
    Co-authored-by: jkgoodrich <33063077+jkgoodrich@users.noreply.github.com>

--- gnomad/resources/grch38/reference_data.py
+++ gnomad/resources/grch38/reference_data.py
@@ -324,7 +324,7 @@ methylation_sites = GnomadPublicTableResource(
 
 # Methylation scores range from 0-12.
 methylation_sites_chrx = GnomadPublicTableResource(
-    path="gs://gcp-public-data--gnomad/resources/grch38/methylation_sites/methylation_chrX.ht",
+    path="gs://gnomad-public-requester-pays/resources/grch38/methylation_sites/methylation_chrX.ht",
     import_func=_import_methylation_sites,
     import_args={
         "path": "gs://gcp-public-data--gnomad/resources/grch38/methylation_sites/methylation_chrX.bed",

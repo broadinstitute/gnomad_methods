5edb4b477c55a5ed848c19e5b0a132e116afc86b
jkgoodrich <33063077+jkgoodrich@users.noreply.github.com>
2023-11-13 12:17:33 -0700

    Add an extra newline

--- gnomad/utils/transcript_annotation.py
+++ gnomad/utils/transcript_annotation.py
@@ -22,11 +22,12 @@ def summarize_rsem_mt(
 
         The output can be returned in one of the following formats (both keyed by
         "transcript_id" and "gene_id"):
-            - A Table with an 'rsem' field containing an array of summarized expression
-              values by tissue, where the order of tissues in the array is indicated by
-              the "tissues" global annotation (`tissue_as_row` set to False).
-            - A Table with a row annotation for each tissue containing the summarized
-              tissue expression value (`tissue_as_row` set to True).
+
+        - A Table with an 'rsem' field containing an array of summarized expression
+          values by tissue, where the order of tissues in the array is indicated by
+          the "tissues" global annotation (`tissue_as_row` set to False).
+        - A Table with a row annotation for each tissue containing the summarized
+          tissue expression value (`tissue_as_row` set to True).
 
     :param rsem_mt: MatrixTable of RSEM quantifications.
     :param tissue_expr: Column expression indicating tissue type.

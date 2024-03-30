88c0d5b94023eeaf2e84631f3b7f398808e4e35b
KoalaQin <heqin@broadinstitute.org>
2024-03-29 20:49:25 -0400

    Delete data_type from fafmax info dict

--- gnomad/utils/vcf.py
+++ gnomad/utils/vcf.py
@@ -972,18 +972,6 @@ def make_info_dict(
                 ),
             },
         }
-        if prefix == "joint_" or suffix == "_joint":
-            fafmax_dict.update(
-                {
-                    f"{prefix}fafmax{label_delimiter}data{label_delimiter}type{suffix}": {
-                        "Number": "A",
-                        "Description": (
-                            "Data type with maximum filtering allele frequency"
-                            f" {description_text}"
-                        ),
-                    },
-                }
-            )
 
         info_dict.update(fafmax_dict)
 

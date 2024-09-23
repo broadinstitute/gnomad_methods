5c74366af4869140c0965ac011de0aca0bf2528a
klaricch <kristen@broadinstitute.org>
2024-09-23 13:06:45 -0400

    Update gnomad/utils/sparse_mt.py
    Co-authored-by: jkgoodrich <33063077+jkgoodrich@users.noreply.github.com>

--- gnomad/utils/sparse_mt.py
+++ gnomad/utils/sparse_mt.py
@@ -196,13 +196,15 @@ def _get_info_agg_expr(
     :param array_sum_agg_fields: Fields to aggregate using element-wise summing over an
         array.
     :param prefix: Optional prefix for the fields. Used for adding 'AS_' in the AS case.
-    :param treat_fields_as_allele_specific: Treat info fields as allele-specific. Defaults to False.
-    :param retain_cdfs: If True, retains the cumulative distribution functions (CDFs) as an annotation
-        for `median_agg_fields`. Keeping the CDFs is useful for annotations that require calculating the median
-        across combined datasets at a later stage. Default is False.
-    :param cdf_k: Parameter controlling the accuracy vs. memory usage tradeoff when retaining CDFs. A higher
-        value of `cdf_k` results in a more accurate CDF approximation but increases memory usage and computation
-        time. Default is 200.
+    :param treat_fields_as_allele_specific: Treat info fields as allele-specific. 
+        Defaults to False.
+    :param retain_cdfs: If True, retains the cumulative distribution functions (CDFs) 
+        as an annotation for `median_agg_fields`. Keeping the CDFs is useful for 
+        annotations that require calculating the median across combined datasets at a 
+        later stage. Default is False.
+    :param cdf_k: Parameter controlling the accuracy vs. memory usage tradeoff when 
+        retaining CDFs. A higher value of `cdf_k` results in a more accurate CDF 
+        approximation but increases memory usage and computation time. Default is 200.
     :return: Dictionary of expression names and their corresponding aggregation
         Expression.
     """

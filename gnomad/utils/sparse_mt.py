4d03a9d51a060d06460f833280105914419a0909
Kristen Laricchia <kristen@broadinstitute.org>
2024-09-23 13:08:01 -0400

    black

--- gnomad/utils/sparse_mt.py
+++ gnomad/utils/sparse_mt.py
@@ -196,14 +196,14 @@ def _get_info_agg_expr(
     :param array_sum_agg_fields: Fields to aggregate using element-wise summing over an
         array.
     :param prefix: Optional prefix for the fields. Used for adding 'AS_' in the AS case.
-    :param treat_fields_as_allele_specific: Treat info fields as allele-specific. 
+    :param treat_fields_as_allele_specific: Treat info fields as allele-specific.
         Defaults to False.
-    :param retain_cdfs: If True, retains the cumulative distribution functions (CDFs) 
-        as an annotation for `median_agg_fields`. Keeping the CDFs is useful for 
-        annotations that require calculating the median across combined datasets at a 
+    :param retain_cdfs: If True, retains the cumulative distribution functions (CDFs)
+        as an annotation for `median_agg_fields`. Keeping the CDFs is useful for
+        annotations that require calculating the median across combined datasets at a
         later stage. Default is False.
-    :param cdf_k: Parameter controlling the accuracy vs. memory usage tradeoff when 
-        retaining CDFs. A higher value of `cdf_k` results in a more accurate CDF 
+    :param cdf_k: Parameter controlling the accuracy vs. memory usage tradeoff when
+        retaining CDFs. A higher value of `cdf_k` results in a more accurate CDF
         approximation but increases memory usage and computation time. Default is 200.
     :return: Dictionary of expression names and their corresponding aggregation
         Expression.
@@ -545,12 +545,12 @@ def get_site_info_expr(
     :param sum_agg_fields: Fields to aggregate using sum.
     :param int32_sum_agg_fields: Fields to aggregate using sum using int32.
     :param median_agg_fields: Fields to aggregate using (approximate) median.
-    :param retain_cdfs: If True, retains the cumulative distribution functions (CDFs) 
-        as an annotation for `median_agg_fields`. Keeping the CDFs is useful for 
-        annotations that require calculating the median across combined datasets at a 
+    :param retain_cdfs: If True, retains the cumulative distribution functions (CDFs)
+        as an annotation for `median_agg_fields`. Keeping the CDFs is useful for
+        annotations that require calculating the median across combined datasets at a
         later stage. Default is False.
-    :param cdf_k: Parameter controlling the accuracy vs. memory usage tradeoff when 
-        retaining CDFs. A higher value of `cdf_k` results in a more accurate CDF 
+    :param cdf_k: Parameter controlling the accuracy vs. memory usage tradeoff when
+        retaining CDFs. A higher value of `cdf_k` results in a more accurate CDF
         approximation but increases memory usage and computation time. Default is 200.
     :return: Expression containing the site-level info fields
     """
@@ -628,12 +628,12 @@ def default_compute_info(
         for VQSR.
     :param ac_filter_groups: Optional dictionary of sample filter expressions to compute
         additional groupings of ACs. Default is None.
-    :param retain_cdfs: If True, retains the cumulative distribution functions (CDFs) 
-        as an annotation for `median_agg_fields`. Keeping the CDFs is useful for 
-        annotations that require calculating the median across combined datasets at a 
+    :param retain_cdfs: If True, retains the cumulative distribution functions (CDFs)
+        as an annotation for `median_agg_fields`. Keeping the CDFs is useful for
+        annotations that require calculating the median across combined datasets at a
         later stage. Default is False.
-    :param cdf_k: Parameter controlling the accuracy vs. memory usage tradeoff when 
-        retaining CDFs. A higher value of `cdf_k` results in a more accurate CDF 
+    :param cdf_k: Parameter controlling the accuracy vs. memory usage tradeoff when
+        retaining CDFs. A higher value of `cdf_k` results in a more accurate CDF
         approximation but increases memory usage and computation time. Default is 200.
     :return: Table with info fields
     :rtype: Table

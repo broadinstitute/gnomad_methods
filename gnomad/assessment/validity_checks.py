2153896f5db12767c334c4e5be413fa99b94aefc
Kristen Laricchia <kristen@broadinstitute.org>
2024-12-17 11:13:58 -0600

    small edit

--- gnomad/assessment/validity_checks.py
+++ gnomad/assessment/validity_checks.py
@@ -1220,7 +1220,8 @@ def check_missingness_of_struct(
     Recursively check the fraction of missing values of all fields within a StructExpression.
 
     Either a standalone or nested Struct can be provided. If the struct contains on array of values, the array
-    will be considered missing if it is NA, an empty array, or only has missing elements.
+    will be considered missing if it is NA, an empty array, or only has missing 
+    elements.
 
     :param struct_expr: StructExpression for which to check for missing values.
     :param prefix: A prefix to append to names of struct fields within the struct_expr.

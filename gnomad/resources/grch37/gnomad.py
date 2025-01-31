68f9c75af230771b2b76326f58b157d4f5319c4d
Katherine Chao <kchao@broadinstitute.org>
2025-01-31 12:50:33 -0500

    Update gnomad/resources/grch37/gnomad.py
    Co-authored-by: klaricch <kristen@broadinstitute.org>

--- gnomad/resources/grch37/gnomad.py
+++ gnomad/resources/grch37/gnomad.py
@@ -164,7 +164,7 @@ def _public_mnv_path(distance: str = "1") -> str:
     :return: Path to MNV data.
     :raises DataException: If the provided distance is invalid (outside 1-10).
     """
-    distances = set(map(str, range(1, 10)))
+    distances = set(map(str, range(1, 11)))
     if distance not in distances:
         raise DataException(
             f"Invalid distance: '{distance}'. Valid options are {distances}."

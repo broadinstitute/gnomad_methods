f9a625b70009f86e070dbdc244dc656bcb4b1064
Qin He <44242118+KoalaQin@users.noreply.github.com>
2025-02-06 11:35:26 -0500

    Black

--- tests/sample_qc/test_de_novo.py
+++ tests/sample_qc/test_de_novo.py
@@ -25,26 +25,96 @@ class TestDeNovoMutation:
     @pytest.mark.parametrize(
         "proband_pl, father_pl, mother_pl, diploid, hemi_x, hemi_y, freq_prior, min_pop_prior, expected_p_dn",
         [
-            ([73, 0, 161], [0, 99, 198], [0, 99, 198], True, False, False, 1.80e-02,
-             100 / 3e7, 0.999),
-            ([152, 0, 283], [0, 99, 198], [0, 63, 126], True, False, False, 7.55e-02,
-             100 / 3e7, 0.198),
-            ([99, 50, 0], [0, 99, 198], [0, 99, 198], False, True, False, 0.005,
-             100 / 3e7, 1),
-            ([2326, 140, 0], [0, 40, 80], [0, 40, 80], False, False, True, 1.97e-04,
-             100 / 3e7, 0.297),
-            ([99, 50, 0], [0, 99, 198], [0, 99, 198], False, True, False, 0.005,
-             100 / 3e7, 1),
-            ([99, 50, 0], [0, 99, 198], [0, 99, 198], False, False, True, 0.005,
-             100 / 3e7, 1),
-            ([2, 0, 230], [0, 0, 0], [0, 0, 0], True, False, False, 2.03e-02,
-             100 / 3e7, 0)
-        ]
+            (
+                [73, 0, 161],
+                [0, 99, 198],
+                [0, 99, 198],
+                True,
+                False,
+                False,
+                1.80e-02,
+                100 / 3e7,
+                0.999,
+            ),
+            (
+                [152, 0, 283],
+                [0, 99, 198],
+                [0, 63, 126],
+                True,
+                False,
+                False,
+                7.55e-02,
+                100 / 3e7,
+                0.198,
+            ),
+            (
+                [99, 50, 0],
+                [0, 99, 198],
+                [0, 99, 198],
+                False,
+                True,
+                False,
+                0.005,
+                100 / 3e7,
+                1,
+            ),
+            (
+                [2326, 140, 0],
+                [0, 40, 80],
+                [0, 40, 80],
+                False,
+                False,
+                True,
+                1.97e-04,
+                100 / 3e7,
+                0.297,
+            ),
+            (
+                [99, 50, 0],
+                [0, 99, 198],
+                [0, 99, 198],
+                False,
+                True,
+                False,
+                0.005,
+                100 / 3e7,
+                1,
+            ),
+            (
+                [99, 50, 0],
+                [0, 99, 198],
+                [0, 99, 198],
+                False,
+                False,
+                True,
+                0.005,
+                100 / 3e7,
+                1,
+            ),
+            (
+                [2, 0, 230],
+                [0, 0, 0],
+                [0, 0, 0],
+                True,
+                False,
+                False,
+                2.03e-02,
+                100 / 3e7,
+                0,
+            ),
+        ],
     )
-
     def test_calculate_de_novo_post_prob(
-            self, proband_pl, father_pl, mother_pl, diploid, hemi_x, hemi_y, freq_prior,
-            min_pop_prior, expected_p_dn
+        self,
+        proband_pl,
+        father_pl,
+        mother_pl,
+        diploid,
+        hemi_x,
+        hemi_y,
+        freq_prior,
+        min_pop_prior,
+        expected_p_dn,
     ):
         """Test `calculate_de_novo_post_prob` function."""
         # Compute posterior probability of de novo mutation
@@ -60,7 +130,7 @@ class TestDeNovoMutation:
         )
 
         # Assert with floating-point tolerance
-        assert round(hl.eval(p_dn_expr), 3)  == expected_p_dn
+        assert round(hl.eval(p_dn_expr), 3) == expected_p_dn
 
     def test_default_get_de_novo_expr_fail_conditions(self):
         """Test default_get_de_novo_expr with a failing case where multiple fail conditions apply."""
@@ -69,7 +139,9 @@ class TestDeNovoMutation:
         alleles = hl.literal(["A", "C"])
 
         # Define proband, father, and mother genotype structures
-        proband_expr = hl.struct(GT=hl.call(0, 1), AD=[9, 2], DP=11, GQ=2, PL=[2, 0, 230])
+        proband_expr = hl.struct(
+            GT=hl.call(0, 1), AD=[9, 2], DP=11, GQ=2, PL=[2, 0, 230]
+        )
         father_expr = hl.struct(GT=hl.call(0, 0), AD=[0, 0], DP=0, GQ=0, PL=[0, 0, 0])
         mother_expr = hl.struct(GT=hl.call(0, 0), AD=[0, 0], DP=0, GQ=0, PL=[0, 0, 0])
 
@@ -79,8 +151,13 @@ class TestDeNovoMutation:
 
         # Compute de novo classification
         result_expr = default_get_de_novo_expr(
-            locus, alleles, proband_expr, father_expr, mother_expr, is_xx_expr,
-            freq_prior_expr
+            locus,
+            alleles,
+            proband_expr,
+            father_expr,
+            mother_expr,
+            is_xx_expr,
+            freq_prior_expr,
         )
 
         # Expected result structure
@@ -88,7 +165,9 @@ class TestDeNovoMutation:
             is_de_novo=True,
             p_de_novo=hl.missing(hl.tfloat64),
             confidence=hl.missing(hl.tstr),
-            fail_reason=hl.set({"min_de_novo_p", "min_proband_ab", "min_proband_gq", "parent_sum_ad_0"}),
+            fail_reason=hl.set(
+                {"min_de_novo_p", "min_proband_ab", "min_proband_gq", "parent_sum_ad_0"}
+            ),
         )
 
         # Evaluate Hail expressions to convert to Python-native objects

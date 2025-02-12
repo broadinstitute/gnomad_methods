fdda37ffb74775cfaefeda1c7e4fe3f878b90e23
Kristen Laricchia <kristen@broadinstitute.org>
2025-02-12 14:13:30 -0500

    (info -> warn)

--- gnomad/assessment/validity_checks.py
+++ gnomad/assessment/validity_checks.py
@@ -966,7 +966,7 @@ def compute_missingness(
                 n_missing,
                 (100 * n_missing / n_sites),
             )
-    logger.info("%d missing metrics checks failed", n_fail)
+    logger.warning("%d missing metrics checks failed", n_fail)
 
 
 def vcf_field_check(

a112ba4dfb13a24249f52b103212fbd4a7290fdb
Katherine Chao <kchao@broadinstitute.org>
2025-08-22 14:14:04 -0400

    Apply suggestions from code review
    Co-authored-by: Mike Wilson <mwilson@broadinstitute.org>

--- gnomad/utils/annotations.py
+++ gnomad/utils/annotations.py
@@ -1917,13 +1917,12 @@ def annotate_downsamplings(
     else:
         t = t.annotate(downsampling=ht[t.s])
 
+    t = t.annotate_globals(downsamplings=downsamplings)
+    
     if gen_anc_counts:
-        return t.annotate_globals(
-            downsamplings=downsamplings,
-            ds_gen_anc_counts=gen_anc_counts,
-        )
+        t = t.annotate_globals(ds_gen_anc_counts=gen_anc_counts)
 
-    return t.annotate_globals(downsamplings=downsamplings)
+    return t
 
 
 def build_freq_stratification_list(
